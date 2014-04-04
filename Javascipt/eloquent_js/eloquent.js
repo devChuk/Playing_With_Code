275db63410ba65f57184e63fd737c34659d99e5c
Brian Chuk <brian_chuk@yahoo.com>
2014-04-04 19:27:02 -0400

    progress

--- Javascipt/eloquent_js/eloquent.js
+++ Javascipt/eloquent_js/eloquent.js
@@ -1,9 +1,28 @@
 function greaterThan(num) {
  return function(y) {
-    return y > x;
+    return y > 1;
   };
 }
 
 var greaterThanTen = greaterThan(10);
-show(greaterThanTen(9));
+//console.log(greaterThanTen(9));
+
+
+
+var set = {"Spot": true};
+set["White Fang"] = true;
+delete set["Spot"];
+//console.log("LuLu" in set);
+
+
+function range(num) {
+	var list = new Array();
+	for (var i = 0; i <= num; i++)
+		list.push(i);
+	list.forEach(function(entry) {
+		console.log(entry)
+	});
+}
+//console.log(range(10));
+
 
