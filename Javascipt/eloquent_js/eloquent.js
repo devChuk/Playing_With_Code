efa65a5ddde2fbd1e8958b474270ae27447ed72d
Brian Chuk <brian_chuk@yahoo.com>
2014-04-08 11:13:33 -0400

    yayyy

--- Javascipt/eloquent_js/eloquent.js
+++ Javascipt/eloquent_js/eloquent.js
@@ -31,6 +31,15 @@ function startsWith(word,prefix) {
 }
 
 function catNames(paragraph) {
-	paragraph.slice(paragraph.indexOf(":"));
-	
+  var colon = paragraph.indexOf(":");
+  return paragraph.slice(colon + 2).split(", ");
 }
+
+/*
+var paragraph = "born 20/09/2004 (mother Yellow Bess): " +
+              	"    Doctor Hobbles the 2nd, Noog"
+console.log(paragraph);
+console.log("PROCESSING");
+console.log(catNames(paragraph));
+*/
+
