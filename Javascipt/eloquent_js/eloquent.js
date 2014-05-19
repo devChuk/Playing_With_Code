037511d09961fa566ebad91421b2bc04ab2f70e1
Brian Chuk <brian_chuk@yahoo.com>
2014-05-19 09:01:59 -0400

    back to js gogo

--- Javascipt/eloquent_js/eloquent.js
+++ Javascipt/eloquent_js/eloquent.js
@@ -138,3 +138,17 @@ function count(test,array) {
   }, 0, array);
 }
 
+function processParagraph(paragraph) {
+  var header = 0;
+  while (paragraph.charAt(0) == "%") {
+    paragraph = paragraph.slice(1);
+    header++;
+  }
+
+  return {type: (header == 0 ? "p" : "h" + header),
+          content: paragraph};
+}
+
+function splitParagraph(paragraph) {
+  
+}
