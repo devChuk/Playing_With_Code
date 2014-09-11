77598a807e037a6bb4603632674076d5ffd9bf7e
Brian Chuk <brian_chuk@yahoo.com>
2014-09-11 13:02:10 -0400

    Im afraid that they wont let me take the labs early

---
+++ class/CS1124/lab/rec02/hydrocarbonSort.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+using namespace std;
+
+struct HydroCarbon {
+	string name;    			//the name of the hydrocarbon. e.g. Propyne
+	string formula;				//the chemical formula of the hydrocarbon. e.g. C3H3	
+	int position;				//the 
+};
+
+void searchHydrocarbon() {		//finds a hydrocarbon
+
+}
+
+void readAndProcessFile(ifstream ifs, vector<HydroCarbon> molecules) {
+								//the name is rather self-explanatory
+}
+
+void displayHydroCarbons(vector<HydroCarbon>& molecules) {
+
+}
+
+int main() {
+
+}
