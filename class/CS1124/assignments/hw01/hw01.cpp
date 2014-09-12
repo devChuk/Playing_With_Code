aa867580242faa32a6d9a8ecc8bfee30c273068c
Brian Chuk <brian_chuk@yahoo.com>
2014-09-11 20:32:07 -0400

    yo swagger swagger

--- class/CS1124/assignments/hw01/hw01.cpp
+++ class/CS1124/assignments/hw01/hw01.cpp
@@ -9,6 +9,7 @@ Status lists all warriors, alive or dead, and their strengths.*/
 #include <vector>
 #include <string>
 #include <fstream>
+#include <stdlib.h>
 using namespace std;
 
 struct Warrior {
@@ -19,29 +20,102 @@ struct Warrior {
 
 
 void warrior(vector<Warrior>& warriors, string name, int strength) {
+	//creates a new warrior with the specified name and strength
 	Warrior newWarrior;
 	newWarrior.name = name;
 	newWarrior.strength = strength;
 
 	warriors.push_back(newWarrior);
-	//creates a new warrior with the specified name and strength
 }
 
-void battle(vector<Warrior>& warriors, string name1, string name2) {
+int searchWarrior(vector<Warrior>& warriors, string name) {
+	//searches for a warrior in the warriors vector and returns an int of its location.
+	int index = -1;
+	for (int i = 0; i < warriors.size(); i++) {
+		if (warriors[i].name == name) {
+			index = i;
+			break;
+		}
+		return i;
+	}
+}
 
+int battle(vector<Warrior>& warriors, string name1, string name2) {
 	//causes a battle between two warriors
+	int a = searchWarrior(warriors, name1);
+	int b = searchWarrior(warriors, name2);
+	if (warriors[a].strength == 0 && warriors[b].strength == 0) {
+		cout << "Oh, NO! They're both dead! Yuck!" << endl;
+		return 0;
+	}
+	cout << warriors[a].name << " battles " << warriors[b].name << endl;
+
+	if (warriors[a].strength > warriors[b].strength) {
+	//if warrior A is stronger
+		warriors[a].strength -= warriors[b].strength;
+		warriors[b].strength = 0;
+		if (warriors[a].name == "Jim") {
+			cout << "He's dead Jim" << endl;
+		}
+		else {
+			cout << warriors[a].name << " defeats " << warriors[b].name << endl;
+		}
+	}
+	else if (warriors[b].strength > warriors[a].strength) {
+	//if warrior B is stronger
+		warriors[b].strength -= warriors[a].strength;
+		warriors[a].strength = 0;
+		if (warriors[b].name == "Jim") {
+			cout << "He's dead Jim" << endl;
+		}
+		else {
+			cout << warriors[b].name << " defeats " << warriors[a].name << endl;
+		}
+	}
+	else {
+	//if they are equally matched
+		warriors[a].strength = 0;
+		warriors[b].strength = 0;
+		cout << "Mutual Annihalation: " << warriors[a].name << " and " << warriors[b].name << " die at each other's hands" << endl;
+	}
 }
 
 void status(vector<Warrior> warriors) {
 	//lists all warriors, alive or dead, and their strengths
+	cout << "There are: " << warriors.size() << " warriors\n";
+	for (int i = 0; i < warriors.size(); i++) {
+		cout << "Warrior: " << warriors[i].name << ", strength: " << warriors[i].strength << endl;
+	}
 }
 
-void readandprocessFile(ifstream ifs, vector<Warrior> warriors){
+void readandprocessFile(ifstream& ifs, vector<Warrior>& warriors){
 	//opens warriors.txt and processes the information
+	string s;
+	string name;
+	string name2;
+	int strength;
+	while(ifs >> s) {
+		if (s == "Warrior") {
+			ifs >> name;
+			ifs >> strength;
+			warrior(warriors, name, strength);
+		}
+		else if (s == "Battle") {
+			ifs >> name;
+			ifs >> name2;
+			battle(warriors, name, name2);
+		}
+		else if (s == "Status") {
+			status(warriors);
+		}
+		else {
+			cout << s << ": command not found\n";
+		}
+	}
 }
 
 int main() {
-	//The main function that pulls it all together. What didja expect?
+	//The main function that pulls it all together.
 	ifstream ifs("warriors.txt");
 	vector<Warrior> warriors;
 	readandprocessFile(ifs, warriors);
