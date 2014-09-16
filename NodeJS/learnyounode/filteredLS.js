532c4c1b51d593f9c65f82953f49d00b027205bc
Brian Chuk <brian_chuk@yahoo.com>
2014-09-16 15:10:55 -0400

    filtered ls command done

--- NodeJS/learnyounode/filteredLS.js
+++ NodeJS/learnyounode/filteredLS.js
@@ -11,3 +11,33 @@ The list of files should be printed to the console, one file per line.
 You must use asynchronous I/O.
 */
 
+var fs = require('fs');
+var array = [];
+var filter = false;
+if (process.argv[3])
+	filter = true;
+//console.log((((fs.readFileSync(process.argv[2])).toString()).split('\n')).length - 1);
+fs.readdir(process.argv[2], function(err, array) {
+	if (err) throw err;
+	for (var i = 0; i < array.length; i++) {
+		if (filter) {
+			if (array[i].indexOf("." + process.argv[3]) != -1)
+				console.log(array[i]);
+			}
+		else {
+			console.log(array[i]);
+		}
+	}
+});
+
+/*Official solution
+var fs = require('fs')
+var path = require('path')
+
+fs.readdir(process.argv[2], function (err, list) {
+  list.forEach(function (file) {
+    if (path.extname(file) === '.' + process.argv[3])
+      console.log(file)
+  })
+})
+*/
