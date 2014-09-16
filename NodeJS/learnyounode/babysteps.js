df18c9a2aac5ce3d00a633322d34748aeed1fca3
Brian Chuk <brian_chuk@yahoo.com>
2014-09-16 14:24:18 -0400

    baby steps

---
+++ NodeJS/learnyounode/babysteps.js
@@ -0,0 +1,15 @@
+//A program that accepts one or more numbers as command-line arguments and prints the sum of those numbers to the console (stdout).
+//console.log(process.argv);
+
+function sumAll(args) {
+	var sum = 0;
+	for (i = 2; i < args.length; i++) {
+		if (parseInt(args[i]) != NaN) {
+			sum += parseInt(args[i]);
+		}
+	}
+	return sum;
+}
+
+
+console.log(sumAll(process.argv));
