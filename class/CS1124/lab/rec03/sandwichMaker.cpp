7312586e803c67a3d328e935f0b84da3f074f8d6
Brian Chuk <brian_chuk@yahoo.com>
2014-09-18 14:10:04 -0400

    done. my compiler somehow doesnt work = =

--- class/CS1124/lab/rec03/sandwichMaker.cpp
+++ class/CS1124/lab/rec03/sandwichMaker.cpp
@@ -1,13 +1,25 @@
+/*
+Brian Chuk
+bc1691
+9/18/14
+
+lab03
+
+We have just been hired by a sandwich making company to write modeling code for them.
+Specifically they are interested in the order taking, sandwich making, delivery truck loading and delivery and return of the delivery trucks. We'll do this in steps, of course, as we always think incremental development.
+
+Our job is to create software tools that the company's programmers can use.
+In testing our code, we'll be writing code a lot like what their programmers might write but our job is really only to provide the things they will need.
+*/
+
 #include <iostream>
 #include <vector>
 #include <string>
-using namespace std;
+	using namespace std;
 
 class Sandwich {
 	public:
-		Sandwich(int b = 2, int c = 1, int t = 2, int ma = 0.1, int mu = 0.05):
-			tomato(t), cheese(c), mustard(mu), mayo(ma), bread(b){
-		}
+		Sandwich(int c = 1, int t = 2, int ma = 0.1, int mu = 0.05): tomato(t), cheese(c), mustard(mu), mayo(ma), bread(2){}
 
 		void display(ostream& os = cout) const{
 			cout<< "bread: " << bread 
@@ -24,17 +36,44 @@ class Sandwich {
 		int getMustard() const{return mustard;}
 
 		//mutators
-		int setBread(int newB) {return bread = newB;}
 		int setCheeese(int newC) {return cheese = newC;}
 		int setTomato(int newT) {return tomato = newT;}
 		int setMayo(int newMa) {return mayo = newMa;}
 		int setMustard(int newMu) {return mustard = newMu;}
+
 	private:
-		int bread, cheese, tomato;
+		const int bread;
+		int cheese, tomato;
 		double mayo, mustard;
 };
 
-int main(){
-	Sandwich sandwich;
-	sandwich.display();
+class Truck {
+	public:
+		void insertSandwich(const Sandwich& s) {
+			sandwiches.push_back(s);
+		}
+
+		void checkBeforeLeaving() const{
+			for (int i = 0; i < sandwiches.size(); i++) {
+				sandwiches[i].display();
+			}
+		}
+	private:
+		vector<Sandwich> sandwiches;
+};
+
+int main() {
+	Truck truck;
+	Sandwich one;
+	Sandwich two;
+	two.setMustard(0);
+	Sandwich three = Sandwich(2, 2, 0.1, 0.05);
+	Sandwich four;
+
+	truck.insertSandwich(one);
+	truck.insertSandwich(two);
+	truck.insertSandwich(three);
+	truck.insertSandwich(four);
+	truck.checkBeforeLeaving();
+	two.getMustard();
 }
