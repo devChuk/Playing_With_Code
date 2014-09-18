0307b8d9407ac2d13e246868b77071140f308856
Brian Chuk <brian_chuk@yahoo.com>
2014-09-18 12:44:11 -0400

    reordered variables in order of most changed to least changed

--- class/CS1124/lab/rec03/sandwichMaker.cpp
+++ class/CS1124/lab/rec03/sandwichMaker.cpp
@@ -5,10 +5,18 @@ using namespace std;
 
 class Sandwich {
 	public:
-		Sandwich(int bread = 2, int cheese = 1, int tomato = 2, int mayo = 0.1, int mustard= 0.05) {}
-		void display(ostream os = cout) const {
-			os << "bread: " << bread << "\ncheese: " << cheese << "\ntomato: " << tomato << "\nmayo: " << mayo << "\nmustard" << mustard << endl;
+		Sandwich(int b = 2, int c = 1, int t = 2, int ma = 0.1, int mu = 0.05):
+			tomato(t), cheese(c), mustard(mu), mayo(ma), bread(b){
 		}
+
+		void display(ostream& os = cout) const{
+			cout<< "bread: " << bread 
+				<< "\ncheese: " << cheese 
+				<< "\ntomato: " << tomato 
+				<< "\nmayo: " << mayo 
+				<< "\nmustard" << mustard << endl;
+		}
+
 	private:
 		int bread, cheese, tomato;
 		double mayo, mustard;
