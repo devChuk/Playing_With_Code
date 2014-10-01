df158f6f37f693dc232bdad940b4a819ef5d7083
Brian Chuk <brian_chuk@yahoo.com>
2014-10-01 17:24:43 -0400

    noble and warrior classes

--- class/CS1124/assignments/hw03/hw03.cpp
+++ class/CS1124/assignments/hw03/hw03.cpp
@@ -1,1 +1,77 @@
 /*We will model a game of medieval times. Our world is filled with nobles and warriors. Nobles don't have much to do except do battle with each other. (We'll leave the feasting and other entertainments for add-ons.) Warriors don't have much to do except hire out to a noble and fight in his behalf. Of course the nobles are pretty wimpy themselves and will lose if they don't have warriors to defend them.*/
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <stdlib.h>
+using namespace std;
+
+class Warrior {
+public:
+	string name;
+	int strength;
+
+	Warrior(const string newName, const int newStrength)
+		:name(newName), strength(newStrength) {}
+};
+
+class Noble {
+public:
+	string name;
+
+	Noble(const string& newName)
+		: name(newName) {}
+
+	void hire(Warrior& warrior) {}
+
+	void fire(Warrior& warrior) {}
+
+	void display() {}
+
+	void battle(Noble target) {}
+
+private:
+
+	vector<Warrior*> warriors;
+
+};
+
+
+int main() {
+	Noble art("King Arthur");
+	Noble lance("Lancelot du Lac");
+	Noble jim("Jim");
+	Noble linus("Linus Torvalds");
+	Noble billie("Bill Gates");
+
+	Warrior cheetah("Tarzan", 10);
+	Warrior wizard("Merlin", 15);
+	Warrior theGovernator("Conan", 12);
+	Warrior nimoy("Spock", 15);
+	Warrior lawless("Xena", 20);
+	Warrior mrGreen("Hulk", 8);
+	Warrior dylan("Hercules", 3);
+
+	/*jim.hire(nimoy);
+	lance.hire(theGovernator);
+	art.hire(wizard);
+	lance.hire(dylan);
+	linus.hire(lawless);
+	billie.hire(mrGreen);
+	art.hire(cheetah);
+
+	jim.display();
+	lance.display();
+	art.display();
+	linus.display();
+	billie.display();
+
+	art.fire(cheetah);
+	art.display();
+
+	art.battle(lance);
+	jim.battle(lance);
+	linus.battle(billie);
+	billie.battle(lance);
+	*/
+}
