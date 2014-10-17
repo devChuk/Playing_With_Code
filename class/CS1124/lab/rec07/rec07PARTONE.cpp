2b85f2364928e4ee63c65462e97e12d15d8e4c8f
Brian Chuk <brian_chuk@yahoo.com>
2014-10-17 09:49:20 -0400

    part 2 started

--- class/CS1124/lab/rec07/rec07PARTONE.cpp
+++ class/CS1124/lab/rec07/rec07PARTONE.cpp
@@ -124,93 +124,79 @@ public:
 private:
   vector<Instrument*> inventory;
 };
-// PART TWO
+// PART ONE
 int main()
 {
-  // The initialization phase
 
+  cout << "Define some instruments ----------------------------------------\n";
   Drum drum;
   Cello cello(673);
   Cymbal cymbal;
   Trombone tbone( 4 );
   Trumpet trpt( 12 ) ;
   Violin violin( 567 ) ;
-
+  
+  // use the debugger to look at the mill
+  cout << "Define the MILL ------------------------------------------------\n";
   MILL mill;
+  
+  cout << "Put the instruments into the MILL ------------------------------\n";
   mill.receiveInstr( trpt );
   mill.receiveInstr( violin );
   mill.receiveInstr( tbone );
   mill.receiveInstr( drum );
   mill.receiveInstr( cello );
   mill.receiveInstr( cymbal );
-
-  Musician bob;
-  Musician sue;
-  Musician mary;
-  Musician ralph;
-  Musician jody;
-  Musician morgan;
-
-  // Orch orch;
-
-
-
-  // // THE SCENARIO
-
-  // //Bob joins the orchestra without an instrument.
-  // orch.addPlayer(bob);
-
-  // //The orchestra performs
-  // cout << "orch performs\n";
-  // orch.play();
-
-  // //Sue gets an instrument from the MIL2 and joins the orchestra.
-  // sue.acceptInstr( *mill.loanOut() );
-  // orch.addPlayer(sue);
-
-  // //Ralph gets an instrument from the MIL2.
-  // ralph.acceptInstr( *mill.loanOut() );
-
-  // //Mary gets an instrument from the MIL2 and joins the orchestra.
-  // mary.acceptInstr( *mill.loanOut() );
-  // orch.addPlayer(mary);
-
-  // //Ralph returns his instrument to the MIL2.
-  // mill.receiveInstr( *ralph.giveBackInstr() );
-
-  // //Jody gets an instrument from the MIL2 and joins the orchestra.
-  // jody.acceptInstr( *mill.loanOut() );
-  // orch.addPlayer(jody);
-
-  // // morgan gets an instrument from the MIL2
-  // morgan.acceptInstr( *mill.loanOut() );
-
-  // //The orchestra performs.
-  // cout << "orch performs\n";
-  // orch.play();
-
-  // //Ralph joins the orchestra.
-  // orch.addPlayer(ralph);
-
-  // //The orchestra performs.
-  // cout << "orch performs\n";
-  // orch.play();
   
-  // // bob gets an instrument from the MIL2
-  // bob.acceptInstr( *mill.loanOut() );
+  cout << "Daily test -----------------------------------------------------\n";
+  cout << endl;
+  cout << "dailyTestPlay()" << endl;
+  mill.dailyTestPlay();
+  cout << endl;
+  
+  cout << "Define some Musicians-------------------------------------------\n";
+  Musician j;
+  Musician jj;
+  Musician q;
+  	
+  cout << "TESTING: q.acceptInstr( *mill.loanOut() );-----------------------\n";
+  q.testPlay();	
+  cout << endl;
+  q.acceptInstr( *mill.loanOut() );
+  cout << endl;
+  q.testPlay();
+  cout << endl;
+  cout << "dailyTestPlay()" << endl;
+  mill.dailyTestPlay();
+  
+  cout << endl << endl;
+  
+  q.testPlay();	
+  cout << endl;
+  mill.receiveInstr( *q.giveBackInstr() );
+  j.acceptInstr( *mill.loanOut() );
+  q.acceptInstr( *mill.loanOut() );
+  cout << endl;
+  q.testPlay();
+  cout << endl;
+  j.testPlay();
+  cout << endl;
+  cout << "dailyTestPlay()" << endl;
+  mill.dailyTestPlay();
+  
+  cout << "TESTING: mill.receiveInstr( *q.giveBackInstr() ); -------------\n";
+  
+  // fifth
+  mill.receiveInstr( *q.giveBackInstr() );
+  cout << "TESTING: mill.receiveInstr( *j.giveBackInstr() ); -------------\n";
+  mill.receiveInstr( *j.giveBackInstr() );
 
-  // // ralph gets an instrument from the MIL2
-  // ralph.acceptInstr( *mill.loanOut() );
+  
+  cout << endl;
+  cout << "dailyTestPlay()" << endl;
+  mill.dailyTestPlay();
+  cout << endl;
+  
+  cout << endl;
 
-  // //The orchestra performs.
-  // cout << "orch performs\n";
-  // orch.play();
-
-  // //Morgan joins the orchestra.
-  // orch.addPlayer(morgan);
-
-  // //The orchestra performs.
-  // cout << "orch performs\n";
-  // orch.play();
-
-} // main
+}
