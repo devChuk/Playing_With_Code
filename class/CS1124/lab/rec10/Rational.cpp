6a49fa836ce58f0cc5d4dbf2f23763eee3c438e3
Brian Chuk <brian_chuk@yahoo.com>
2014-11-07 11:44:14 -0500

    gitignore added

--- class/CS1124/lab/rec10/Rational.cpp
+++ class/CS1124/lab/rec10/Rational.cpp
@@ -2,4 +2,16 @@
 #include <vector>
 #include <iostream>
 #include <string>
-        using namespace std;
+	using namespace std;
+
+namespace CS1124 {
+
+
+/////////////////////////////////////////////////////////////////////////
+	istream& operator>>(istream& is, const Rational& num) {
+		//is num, char, denom
+	}
+	ostream& operator<<(ostream& os, const	Rational& num) {
+		
+	}
+}
--- class/CS1124/lab/rec10/Rational.h
+++ class/CS1124/lab/rec10/Rational.h
@@ -6,13 +6,33 @@
 #include <iostream>
 #include <string>
 
+namespace CS1124 {
+
+	class Rational {
+
+	};
 
 
+	std::istream& operator>>(std::istream& is, const Rational& num);
+	std::ostream& operator<<(std::ostream& os, const	Rational& num);
+/*
+Stuff to implement
+<<
+>>
++=
++
+==
+!=
+++
+--
+
+ostream& operator<< (ostream& os, const Elephant& e) {
+    	//os << e.getName();
+    	return os;
+	} 
+
+*/
 
 
-
-
-
-
-
+}
 #endif
