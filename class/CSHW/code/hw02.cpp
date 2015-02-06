eca75faf1f524dd47247f2b9694d959102e4dbde
Brian Chuk <brian_chuk@yahoo.com>
2015-02-06 17:19:31 -0500

    init hw02

---
+++ class/CSHW/code/hw02.cpp
@@ -0,0 +1,17 @@
+//Brian Chuk
+#include <vector>
+#include <string>
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+class trainStopData {
+public:
+
+private:
+	
+};
+
+int main() {
+
+}
