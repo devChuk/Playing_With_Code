cbeac6c63798234f90ebb69ff5d892827c63cc94
Brian Chuk <brian_chuk@yahoo.com>
2015-02-08 18:53:59 -0500

    comments inserted, were done here

--- class/CSHW/code/hw02.cpp
+++ class/CSHW/code/hw02.cpp
@@ -30,8 +30,12 @@ int main() {
 	string stop_id, stop_name;
 	double stop_lat, stop_lon;
 
-	while (getline(ifs,s,',')) {
+	while (getline(ifs,s,',')) { //When getline() has a comma passed in, a comma is used as the delimiter to read up to the comma
 		switch(count) {
+				/*
+				A case statement is used to record the required data into temp variables. An int counter is used to figure out which variable s is holding.
+				So stop_id's are always the first in the line, while stop_name's are the third, and so on..
+				*/
 			case 1:
 				stop_id = s;
 				count++;
@@ -41,14 +45,15 @@ int main() {
 				count++;
 			break;
 			case 5:
-				stop_lat = atof(s.c_str());
+				stop_lat = atof(s.c_str());//for some reason I don't know yet, the function stod() does not recognize doubles in s. So we convert s into a Cstring AND THEN convert it into a double
 				count++;
 			break;
 			case 6:
-				stop_lon = atof(s.c_str());
+				stop_lon = atof(s.c_str()); //see line 48
 				count++;
 			break;
 			case 10:
+				//while the getline above uses commas as a delimeter, it does not use "\n" as a delimeter. Thus, we use the variable newL and substr() to take care of that.
 				newL = s.find("\n");
 				data.push_back(trainStopData(stop_id,stop_name,stop_lat,stop_lon));
 				stop_id = s.substr(newL+1);
@@ -58,6 +63,7 @@ int main() {
 				count++;
 		}
 	}
+	//testing
 	for (int i = 0; i < data.size(); i++) {
 		cout << " ID: " << data[i].get_id() << " NAME: " << data[i].get_stop_name() << " LAT: " << data[i].get_latitude() << " LON: " << data[i].get_longitude() << endl;
 	}
