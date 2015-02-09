bcc9cc818817a288c17604fb144a5f2b3a4cd5cf
Brian Chuk <brian_chuk@yahoo.com>
2015-02-09 18:03:07 -0500

    FIGURED OUT COLORSSS

--- class/CSHW/generate.py
+++ class/CSHW/generate.py
@@ -1,6 +1,6 @@
 import string
 import random
-from PIL import Image, ImageDraw
+from PIL import Image, ImageColor, ImageDraw
 
 
 
@@ -9,14 +9,19 @@ s = raw_input()
 print "your letter is " + s[:1].upper()
 
 
-im = Image.open("output.png")
+im = Image.new("RGB",(800,800),"red")
 
 x, y =  im.size
 eX, eY = 30, 60 #Size of Bounding Box for ellipse
 
-bbox =  (0, 0, x, y)
+box =  [0, 0, x, y]
 draw = ImageDraw.Draw(im)
-draw.rectangle(bbox) # it works, but it needs color
+
+# Image.new("RGB", (600, 600), (250, 250, 400))
+
+# draw.rectangle([0, 0, x, y],fill = 1) # it works, but it needs color
+
+
 del draw
 
 im.save("output.png")
