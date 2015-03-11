df9a1f74d2213b2e67aac1311bbbc493227338b3
Brian Chuk <brian_chuk@yahoo.com>
2015-03-11 19:28:54 -0400

    DONEE

--- Python/calc.py
+++ Python/calc.py
@@ -1,3 +1,17 @@
 while True:
 	math = raw_input("Enter math: ")
-	
+	if ('+' in math):
+		elements = math.partition('+')
+		print int(elements[0]) + int(elements[2])
+	elif '-' in math:
+		elements = math.partition('-')
+		print int(elements[0]) - int(elements[2])
+	elif '/' in math:
+		elements = math.partition('/')
+		print int(elements[0]) - int(elements[2])
+	elif '**' in math:
+		elements = math.partition('**')
+		print int(elements[0]) ** int(elements[2])
+	elif '*' in math:
+		elements = math.partition('*')
+		print int(elements[0]) * int(elements[2])
