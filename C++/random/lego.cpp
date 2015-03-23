e59373de2246b9fbb0104026dca050bca639e6bb
Brian Chuk <brian_chuk@yahoo.com>
2015-03-23 19:01:13 -0400

    init

---
+++ C++/random/lego.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <functional>
+#include <vector>
+#include <string>
+#include <set>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+
+
+int main() {
+
+}
