18c5e8aed7842ed1a919136633351aa5689a4429
Brian Chuk <brian_chuk@yahoo.com>
2015-03-24 00:07:30 -0400

    cleanup and testing

--- C++/random/lego.cpp
+++ C++/random/lego.cpp
@@ -7,10 +7,6 @@
 #include <math.h>
 using namespace std;
 
-//321
-//123
-
-
 string reversi(string input) {
 	if (input.length() <= 1) return input;
 	else return reversi(input.substr(1)) + input.substr(0,1);
@@ -19,11 +15,10 @@ string reversi(string input) {
 void reversi(int input) {
 	if (input < 10) cout << input << endl;
 	else {cout << input % 10; reversi(input / 10);}
-
-	// else return ((input % 10) ) + reversi(input / 10);
 }
 
 
 int main() {
 	reversi(1234);
+	reversi("1234");
 }
