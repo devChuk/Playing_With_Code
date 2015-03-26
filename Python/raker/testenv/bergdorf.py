3f30dee56a69e6b444f4acb5c2de016a96da05a4
Brian Chuk <brian_chuk@yahoo.com>
2015-03-25 21:06:43 -0400

    analyzing system

--- Python/raker/testenv/bergdorf.py
+++ Python/raker/testenv/bergdorf.py
@@ -1,3 +1,20 @@
+## Specification
+# category:
+# brand:
+# gender:
+# name:
+# description:
+# origin: 
+# currency:
+# url:
+# price: int
+# size: []
+# material: []
+# event: []
+# color: []
+# color_family: []
+# images: []
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -5,3 +22,14 @@ from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 import pymongo
 from pymongo import MongoClient
+##########ASK ABOUT MONGODB CREDENTIALS
+#client = MongoClient("mongodb://cris:cris1@c18.candidate.43.mongolayer.com:10018,c333.candidate.40.mongolayer.com:10333/frockhub2")
+#db = client['frockhub2']
+#collection = db['burberry']
+
+def scrape(): #love how there's a single tab for mens and the rest are all for women
+	products = []
+	#browser.find_element_by_xpath()
+	urls = []
+	for url in urls:
+		#startUrl(*url)
