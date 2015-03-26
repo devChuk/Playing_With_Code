cd9deab3ec158b7721ceb3dbce97b416c4c62bff
Brian Chuk <brian_chuk@yahoo.com>
2015-03-26 16:16:43 -0400

    filtering complete for men

--- Python/raker/testenv/bergdorf.py
+++ Python/raker/testenv/bergdorf.py
@@ -52,26 +52,30 @@ driver = webdriver.Firefox() # Create a new instance of the Firefox driver
 """
 
 
+def maleCategoriesfilter(text):
+	return text and "\n" not in text and "DESIGNERS" not in text and "MEN" not in text and text != "SHOES" and text != "BELTS" and "BG" not in text and "NEW" not in text and "BEST" not in text and "MARCUS" not in text and text != "ACCESSORIES"
+
 def grabCategories(pageUrl):
 	driver.get(pageUrl)
 	categories = {}
-	possible = driver.find_elements_by_tag_name("li").toString()
-	print possible
+	possibles = [thing for thing in driver.find_elements_by_tag_name("li") if maleCategoriesfilter(thing.text)]#thing.text and "\n" not in thing.text and "DESIGNERS" not in thing.text] #This piece finds all the possible li's that we need
+	for possible in possibles:
+		print possible
+		print possible.text
 	return categories
 
 def startUrl(url, gender):
 	result = []
 	print "\n"
 	print "scraping from:"
-	print url + " for " + gender
+	print url + " for " + gender + "\n"
 	categories = grabCategories(url)
-
-
 	return result
 
 def scrape(): #love how there's a single tab for mens and the rest are all for women
 	products = []
 	#browser.find_element_by_xpath()
+	#urls = [["http://www.bergdorfgoodman.com/Mens-Store/Clothing/cat000024_cat202802_cat000000/c.cat", "men"], ["http://www.bergdorfgoodman.com/5F-Contemporary/5F-Apparel/Shop-All-5F/cat441205_cat232503_cat230300/c.cat", "women"], ["http://www.bergdorfgoodman.com/Categories/Scarves/cat408112_cat408110_cat408107/c.cat","women"], ["http://www.bergdorfgoodman.com/Shoe-Salon/cat200648/c.cat?siloId=cat200648&navid=topNavShoeSalon","women"], ["http://www.bergdorfgoodman.com/Handbags/cat257221/c.cat?siloId=cat257221&navid=topNavHandbags","women"]]
 	urls = [["http://www.bergdorfgoodman.com/Mens-Store/Clothing/cat000024_cat202802_cat000000/c.cat", "men"]]
 	for url in urls:
 		gender = url.pop()
