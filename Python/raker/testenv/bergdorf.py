ecdc06d9a0888a6b4fa1bd68475d4a2264dc5d0a
Brian Chuk <brian_chuk@yahoo.com>
2015-03-26 20:20:46 -0400

    got them categories

--- Python/raker/testenv/bergdorf.py
+++ Python/raker/testenv/bergdorf.py
@@ -39,18 +39,6 @@ driver = webdriver.Firefox() # Create a new instance of the Firefox driver
 
 
 
-"""
-	browser.get(pageUrl)
-	possible = browser.find_elements_by_tag_name("li")
-	possible = [item for item in possible if 'l-2' in item.get_attribute("class")]
-	possible = [item.find_element_by_tag_name("ul") for item in possible if 'active' in item.find_element_by_tag_name("a").get_attribute("class")][0].find_elements_by_tag_name("li")
-	categories = {}
-	for category in possible:
-		category = category.find_element_by_tag_name("a")
-		categories.update({category.text.lower():category.get_attribute("href")})
-	return categories
-"""
-
 
 def maleCategoriesfilter(text):
 	return text and "\n" not in text and "DESIGNERS" not in text and "MEN" not in text and text != "SHOES" and text != "BELTS" and "BG" not in text and "NEW" not in text and "BEST" not in text and "MARCUS" not in text and text != "ACCESSORIES"
@@ -60,9 +48,26 @@ def grabCategories(pageUrl):
 	categories = {}
 	possibles = [thing for thing in driver.find_elements_by_tag_name("li") if maleCategoriesfilter(thing.text)]#thing.text and "\n" not in thing.text and "DESIGNERS" not in thing.text] #This piece finds all the possible li's that we need
 	for possible in possibles:
-		print possible
+		possible = possible.find_element_by_tag_name("a")
+		print possible.get_attribute("href")
 		print possible.text
+		categories.update({possible.text.lower():possible.get_attribute("href")})
+	print "categories grabbed~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 	return categories
+
+"""
+print categories
+	for page in categories:
+		item_sets = grabItemSets(categories[page])
+		for track in item_sets:
+			for item in item_sets[track]:
+				try:
+					result.append(scrapeProductUrl(item, gender, page, track))
+				except:
+					result.append(scrapeProductUrl(item, gender, page, track))
+	return result
+"""
+
 
 def startUrl(url, gender):
 	result = []
@@ -70,9 +75,10 @@ def startUrl(url, gender):
 	print "scraping from:"
 	print url + " for " + gender + "\n"
 	categories = grabCategories(url)
+	# for page in categories:
 	return result
 
-def scrape(): #love how there's a single tab for mens and the rest are all for women
+def scrape(): #love how there's a single tab for men and the rest are all for women
 	products = []
 	#browser.find_element_by_xpath()
 	#urls = [["http://www.bergdorfgoodman.com/Mens-Store/Clothing/cat000024_cat202802_cat000000/c.cat", "men"], ["http://www.bergdorfgoodman.com/5F-Contemporary/5F-Apparel/Shop-All-5F/cat441205_cat232503_cat230300/c.cat", "women"], ["http://www.bergdorfgoodman.com/Categories/Scarves/cat408112_cat408110_cat408107/c.cat","women"], ["http://www.bergdorfgoodman.com/Shoe-Salon/cat200648/c.cat?siloId=cat200648&navid=topNavShoeSalon","women"], ["http://www.bergdorfgoodman.com/Handbags/cat257221/c.cat?siloId=cat257221&navid=topNavHandbags","women"]]
