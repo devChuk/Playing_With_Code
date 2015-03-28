8e0dcd137e9b8c26204c80fe5e602c11505ab557
Brian Chuk <brian_chuk@yahoo.com>
2015-03-28 02:01:59 -0400

    fixed original/now price issue

--- Python/raker/testenv/bergdorf.py
+++ Python/raker/testenv/bergdorf.py
@@ -40,19 +40,30 @@ materialz = ['silk', 'cotton', 'chiffon', 'satin', 'silt', 'wool', 'linen', 'cas
 
 def getColors():
 	ans = []
-	text = driver.find_element_by_class_name("oneColor").text.lower()
-	images = driver.find_elements_by_class_name('alt-shot')
-	sources = []
-	for image in images:
-		sources.append(image.get_attribute("src"))
-	for color in c_colors:
-		if color in text:
-			ans.append({'name':color, 'color_family': color, 'images': sources})
+	try:
+		print driver.find_element_by_class_name("colorSelectTop.colorSelect").text
+	except:
+		ans = []
+	try:
+		text = driver.find_element_by_class_name("oneColor").text.lower()
+		images = driver.find_elements_by_class_name('alt-shot')
+		sources = []
+		for image in images:
+			sources.append(image.get_attribute("src"))
+		for color in c_colors:
+			if color in text:
+				ans.append({'name':color, 'color_family': color, 'images': sources})
+		return ans
+	except:
+		text = driver.find_elements_by_xpath("//option")
 	return ans
 
 def getc_fam():
 	ans = []
-	text = driver.find_element_by_class_name("oneColor").text.lower()
+	try:
+		text = driver.find_element_by_class_name("oneColor").text.lower()
+	except:
+		text = ""
 	for color in c_colors:
 		if color in text:
 			ans.append(color)
@@ -76,22 +87,25 @@ def getSizes():
 
 def scrapeProduct(productUrl, gender, category):
 	scraped_item = {}
-	if "." in driver.find_element_by_class_name("price").text and "," in driver.find_element_by_class_name("price").text:
-		pprice = int(driver.find_element_by_class_name("price").text.replace(",","")[1:(driver.find_element_by_class_name("price").text.index(".") - 1)])
-		print pprice
+	print driver.find_element_by_id("qvHeaderPrice").text
+	if "." in driver.find_element_by_id("qvHeaderPrice").text and "," in driver.find_element_by_id("qvHeaderPrice").text:
+		try:
+			pprice = int(driver.find_element_by_id("qvHeaderPrice").text.replace(",","")[1:(driver.find_element_by_id("qvHeaderPrice").text.index(".") - 1)])
+		except:
+			#find_element_by_css_selector(".price.pos1");
+			pprice = int(driver.find_element_by_css_selector(".price.pos1").text.replace(",","")[1:(driver.find_element_by_css_selector(".price.pos1").text.index(".") - 1)])
+		print category + " " + str(pprice)
 		scraped_item = {"available": True, "unavailable": {}, "url": productUrl, "gender":gender, "category":category, "currency":"USD", "price": pprice, "brand":driver.find_element_by_id("productDesignerName").text, "name":driver.find_element_by_class_name("displayName").text, "description":driver.find_element_by_class_name("qvTopBorder").text, "color_families":getc_fam(), "materials":getMaterials(), "colors":getColors(), "sizes":getSizes(), "events":[], "attributes":[]}	
-	elif "." in driver.find_element_by_class_name("price").text:
-		scraped_item = {"available": True, "unavailable": {}, "url": productUrl, "gender":gender, "category":category, "currency":"USD", "price":int(driver.find_element_by_class_name("price").text[1:(driver.find_element_by_class_name("price").text.index("."))]), "brand":driver.find_element_by_id("productDesignerName").text, "name":driver.find_element_by_class_name("displayName").text, "description":driver.find_element_by_class_name("qvTopBorder").text, "color_families":getc_fam(), "materials":getMaterials(), "colors":getColors(), "sizes":getSizes(), "events":[], "attributes":[]}	
-		print scraped_item["price"]
-	elif "-$" in driver.find_element_by_class_name("price").text:
-		# if "sweat" in category or "shirt" in category:
+	elif "." in driver.find_element_by_id("qvHeaderPrice").text:
+		scraped_item = {"available": True, "unavailable": {}, "url": productUrl, "gender":gender, "category":category, "currency":"USD", "price":int(driver.find_element_by_id("qvHeaderPrice").text[1:(driver.find_element_by_id("qvHeaderPrice").text.index("."))]), "brand":driver.find_element_by_id("productDesignerName").text, "name":driver.find_element_by_class_name("displayName").text, "description":driver.find_element_by_class_name("qvTopBorder").text, "color_families":getc_fam(), "materials":getMaterials(), "colors":getColors(), "sizes":getSizes(), "events":[], "attributes":[]}	
+		print category + " " + str(scraped_item["price"])
+	elif "-$" in driver.find_element_by_id("qvHeaderPrice").text:
 		scraped_item = {"available": True, "unavailable": {}, "url": productUrl, "gender":gender, "category":category, "currency":"USD", "price":int(driver.find_element_by_class_name("rtlPrice").text[1:]), "brand":driver.find_element_by_id("productDesignerName").text, "name":driver.find_element_by_class_name("lineItemNameSpan").text, "description":driver.find_element_by_class_name("qvTopBorder").text, "color_families":getc_fam(), "materials":getMaterials(), "colors":getColors(), "sizes":getSizes(), "events":[], "attributes":[]}	
-		# else:
-			# count = len(driver.find_elements_by_class_name("rtlPrice"))
-		print scraped_item["price"]
-	elif "," in driver.find_element_by_class_name("price").text:
-		pprice = int(driver.find_element_by_class_name("price").text.replace(",",""))
+		print category + " " + str(scraped_item["price"])
+	elif "," in driver.find_element_by_id("qvHeaderPrice").text:
+		pprice = int(driver.find_element_by_id("qvHeaderPrice").text.text.replace(",",""))
 		scraped_item = {"available": True, "unavailable": {}, "url": productUrl, "gender":gender, "category":category, "currency":"USD", "price": pprice, "brand":driver.find_element_by_id("productDesignerName").text, "name":driver.find_element_by_class_name("displayName").text, "description":driver.find_element_by_class_name("qvTopBorder").text, "color_families":getc_fam(), "materials":getMaterials(), "colors":getColors(), "sizes":getSizes(), "events":[], "attributes":[]}
+		print category + " " + str(scraped_item["price"])
 	return scraped_item
 
 def grabCategoryProducts(pageUrl, gender, page):
@@ -102,17 +116,17 @@ def grabCategoryProducts(pageUrl, gender, page):
 	# while driver.find_element_by_class_name("pagingNav").text == "NEXT":
 	all_products = driver.find_elements_by_class_name("qv-tip")
 	for product in all_products:
-		try:
-			product.click()
-			WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID,'qvViewProductDetails')))
-			prod = scrapeProduct(driver.find_element_by_class_name("prodPageLink").get_attribute("href"), gender, page)
+		# try:
+		product.click()
+		WebDriverWait(driver, 25).until(EC.element_to_be_clickable((By.ID,'qvViewProductDetails')))
+		prod = scrapeProduct(driver.find_element_by_class_name("prodPageLink").get_attribute("href"), gender, page)
 			# bergdorf.insert(prod)##############################################################################################
-		except:	
-			print "SKIPPED ITEM"
+		# except:	
+		# 	print "SKIPPED ITEM " + page
 	return products
 
 def maleCategoriesfilter(text):
-	return text and "\n" not in text and "DESIGNERS" not in text and "MEN" not in text and text != "SHOES" and text != "BELTS" and "BG" not in text and "NEW" not in text and "BEST" not in text and "MARCUS" not in text and "ACCESSORIES" not in text and "ALL" not in text and "MAGAZINE" not in text and "LOOKS" not in text and "STORIES" not in text and "SUN" not in text and "OPTICAL" not in text and "WATCH" not in text
+	return text and "\n" not in text and "DESIGNERS" not in text and "MEN" not in text and text != "SHOES" and text != "BELTS" and "BG" not in text and "NEW" not in text and "BEST" not in text and "MARCUS" not in text and "ACCESSORIES" not in text and "ALL" not in text and "MAGAZINE" not in text and "LOOKS" not in text and "STORIES" not in text and "SUN" not in text and "OPTICAL" not in text and "WATCH" not in text and "SWIM" not in text
 
 def grabCategories(pageUrl):
 	driver.get(pageUrl)
@@ -146,7 +160,8 @@ def scrape(): #love how there's a single tab for men and the rest are all for wo
 	#browser.find_element_by_xpath()
 	#["http://www.bergdorfgoodman.com/Shoe-Salon/cat200648/c.cat?siloId=cat200648&navid=topNavShoeSalon","women"], ["http://www.bergdorfgoodman.com/Handbags/cat257221/c.cat?siloId=cat257221&navid=topNavHandbags","women"]]
 	#urls = [["http://www.bergdorfgoodman.com/Mens-Store/Clothing/cat000024_cat202802_cat000000/c.cat", "men"], ["http://www.bergdorfgoodman.com/5F-Contemporary/5F-Apparel/Shop-All-5F/cat441205_cat232503_cat230300/c.cat", "women"], ["http://www.bergdorfgoodman.com/Categories/Scarves/cat408112_cat408110_cat408107/c.cat","women"]]
-	urls = [["http://www.bergdorfgoodman.com/Handbags/cat257221/c.cat?siloId=cat257221&navid=topNavHandbags","women"]]
+	urls = [["http://www.bergdorfgoodman.com/5F-Contemporary/5F-Apparel/Shop-All-5F/cat441205_cat232503_cat230300/c.cat", "women"]]
+	#DONE: [["http://www.bergdorfgoodman.com/Mens-Store/Clothing/cat000024_cat202802_cat000000/c.cat", "men"]], 
 	for url in urls:
 		gender = url.pop()
 		link = url.pop()
