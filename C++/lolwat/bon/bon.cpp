623b7d7e17d86f701ec23086268d399686c852a5
Brian Chuk <brian_chuk@yahoo.com>
2015-03-28 23:17:06 -0400

    meh

--- C++/lolwat/bon/bon.cpp
+++ C++/lolwat/bon/bon.cpp
@@ -8,10 +8,24 @@
 #include <math.h>
 using namespace std;
 
-void mazeRecurse(vector<vector<char>>) {
+void mazeRecurse(vector<vector<char>> maze, int x, int y) {
+	
+}
 
+void mazeRecurse(vector<vector<char>> maze) {
+	int x = maze.size();
+	int y = maze[0].size();
 }
 
 int main() {
+// xxxxxxxxxxxxxxxxxx
+// s.x...x....x...exx
+// x.x.x.x.xx.x..xxxx
+// x...x...xx.x..x.xx
+// xxx.x..xxx...xx.xx
+// xxx.xx..xx......xx
+// xxx...x.xxxxxxxxxx
+// xxxxx.x........xxx
+// xxxxxxxxxxxxxxxxxx
 	
 }
