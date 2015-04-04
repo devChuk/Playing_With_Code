bb69c601820ed356ca1730b0e4b19dfe1b25f17b
Brian Chuk <brian_chuk@yahoo.com>
2015-04-04 06:01:47 -0400

    Init hw05.cpp//Its currently 6am right now so Im hitting the bed. Night!

---
+++ C++/lolwat/hw05/hw05.cpp
@@ -0,0 +1,14 @@
+//Brian Chuk
+#include <iostream>
+#include <functional>
+#include <vector>
+#include <string>
+#include <set>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+
+
+int main() {
+	
+}
