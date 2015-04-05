312eedf02fa765d382dc8b873784dc3c3ae1c4c3
Brian Chuk <brian_chuk@yahoo.com>
2015-04-05 04:28:07 -0400

    Finishing up remove_last//Code so clean Billy Mays is presenting it in a commercial

--- C++/lolwat/hw05/hw05.cpp
+++ C++/lolwat/hw05/hw05.cpp
@@ -33,13 +33,30 @@ class LListItr {
 public:
     LListItr():current(nullptr){}
     
-    LListItr& operator++();
-    LListItr operator++(int);
+    LListItr& operator++() {
+    	current = current->next;
+    }
+
+    LListItr operator++(int) {
+    	LListItr ans = *this;
+    	++*this;
+    	return ans;
+    }
     
-    bool operator==(const LListItr& rhs);
-    bool operator!=(const LListItr& rhs);
+    bool operator==(const LListItr& rhs) {
+    	if (current == nullptr && rhs.current == nullptr) return true;
+    	if (current == nullptr || rhs.current == nullptr) return false;
+    	return current->element == rhs.current->element && current->next == rhs.current->next;
+    }
+
+
+    bool operator!=(const LListItr& rhs) {
+    	return !(operator==(rhs));
+    }
     
-    Object& operator*();
+    Object& operator*() {
+    	return current->element;
+    }
 
 private:
     LListItr(LListNode<Object>* p) : current(p) {}
@@ -64,8 +81,13 @@ public:
     void push_front(const Object& x) {
     	LListNode<Object>* fresh = new LListNode<Object>();
     	fresh->element = x;
-    	fresh->next = header;
-    	header = fresh;
+    	if (header->next == nullptr) {
+    		header->next = fresh;
+    	}
+    	else {
+    		fresh->next = header->next;
+    		header->next = fresh;	
+    	}
     	s++;
     }
     
@@ -79,16 +101,34 @@ public:
     }
 
     template<class Predicate>// This is how you create a template method within a class
-    void remove_if(Predicate pred);
+    void remove_if(Predicate pred) {
+    	LListItr<Object> p = before_begin();
+    	while (p != end()) {
+    		if (p.current->next != nullptr && pred(*p))
+    			erase_after(p);
+    		p++;
+    	}
+    }
+
+    void remove_last(const Object& x) {
+    	LListItr<Object> target;
+    	LListItr<Object> p = before_begin();
+    	while (p != end()) {
+    		if (p.current->next != nullptr && pred(*p))
+    			erase_after(p);
+    		p++;
+    	}
+    }
 
     LListItr<Object> erase_after(LListItr<Object> itr) {
-    	
+    	itr.current->next = itr.current->next->next;
+    	s--;
     }
 
     void print() {
     	cout << size() << " elements: ";
-    	LListNode<Object>* p = header;
-    	while (p->next != nullptr) {
+    	LListNode<Object>* p = header->next;
+    	while (p != nullptr) {
     		cout << p->element << " ";
     		p = p->next;
     	}
@@ -102,10 +142,17 @@ private:
     int s;
 };
 
-template<class Object> template< class Predicate> /* The signature for when you define the template method outside the class*/
-void LList<Object>::remove_if( Predicate pred) {
+// template<class Object> template< class Predicate> /* The signature for when you define the template method outside the class*/
+// void LList<Object>::remove_if( Predicate pred) {
 
-}
+// }
+
+class LessThanThree { // <3
+public:
+	bool operator()(int x) const {
+		return x < 3;
+	}
+};
 
 int main() {
 	LList<int> list = LList<int>();
@@ -113,8 +160,14 @@ int main() {
 	list.push_front(4);
 	list.push_front(3);
 	list.push_front(2);
-	list.push_front(1);
+	list.push_front(1);						// {1, 2, 3, 4, 5}
 	list.print();
-	list.pop_front();
+	list.pop_front();						// {2, 3, 4, 5}
 	list.print();
+	list.erase_after(list.begin());			// {2, 4, 5}
+	list.print();
+	list.remove_if(LessThanThree());		// {4, 5}
+	list.print();
+
+
 }
