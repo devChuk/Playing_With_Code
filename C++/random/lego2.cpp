cc51ee3933711822ec0f2fe96d0794d2089bce4d
Brian Chuk <brian_chuk@yahoo.com>
2015-04-13 19:26:35 -0400

    init pls halp dont want to study

---
+++ C++/random/lego2.cpp
@@ -0,0 +1,26 @@
+//This is an attempt to study for the upcoming Data Structures test on 4/14
+
+/*STUFF TO STUDY
+- lecture materials
+- redo HW 5 & 6
+
+
+Make sure you know about:
+	writing a method for the LList Class, or LListItr class
+	writing a functor, or generic function
+	using the map class
+	determining the running times
+	knowing how to binary search trees work (similar to the homework)
+	showing you understand the topics we covered in class on the LList class
+	knowing how to go from an infix expression to a postfix expression, and how to evaluate a postfix expression
+*/
+#include <iostream>
+#include <functional>
+#include <vector>
+#include <string>
+#include <set>
+#include <algorithm>
+#include <math.h>
+#include <list>
+using namespace std;
+
