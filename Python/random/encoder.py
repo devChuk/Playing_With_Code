9db461a5d2e75b470f8858d4a7129cd4b8aaabc1
Brian Chuk <brian_chuk@yahoo.com>
2015-04-14 19:10:57 -0400

    encoding stuffs

---
+++ Python/random/encoder.py
@@ -0,0 +1,27 @@
+# text = "Chemistry is an odd class for CS majors to take"
+# text = list(text)
+# for t in text:
+# 	print(ord(t)),
+# will not work in Python 3.0
+
+# text = "80 104 121 115 105 99 115 32 105 115 32 119 101 105 114 100 32 116 111 111"
+# text = text.split(" ")
+# for t in text:
+# 	print(chr(int(t))),
+
+# print hex(int(10100111001))
+
+# text = "Use the .encode() method"
+# print text.encode('base64','strict')
+
+# print "SSBob3BlIHlvdSB1c2VkIHRoZSBkZWNvZGUgbWV0aG9k".decode('base64','strict')
+
+# import urllib
+
+# print urllib.unquote("Wall-E%20%26%20Eve%20are%20totally%20the%20best%20Disney%20characters")
+
+# import hashlib
+
+# m = hashlib.sha1()
+# m.update("magical blubber cats")
+# print m.digest()
