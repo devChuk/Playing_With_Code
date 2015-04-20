ed6e1ada5a0cad7d03fc182c0d57d75eae33ef3d
Brian Chuk <brian_chuk@yahoo.com>
2015-04-20 04:29:07 -0400

    he keeps bothering me

--- C++/lolwat/hw07/hw07.cpp
+++ C++/lolwat/hw07/hw07.cpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <string>
 #include <map>
+#include <list>
 using namespace std;
 
 void parseTrainData(map<string, int>& moo, vector<string>& int_to_stop_id) { //fills up moo & int_to_stop_id
@@ -16,10 +17,29 @@ void parseTrainData(map<string, int>& moo, vector<string>& int_to_stop_id) { //f
 	getline(stopFile, line);
 	int count = 0;
 	while (getline(stopFile, line)) {
-		string stop_id = line.substr(0,line.find(','));
-		moo[stop_id] = count;
-		int_to_stop_id.push_back(stop_id);
-		count++;
+		int comma = line.find(',');
+		string stop_id = line.substr(0,comma);
+		if (stop_id.substr(comma-1) != "S" && stop_id.substr(comma-1) != "N") {
+			moo[stop_id] = count;
+			int_to_stop_id.push_back(stop_id);
+			count++;
+		}
+	}
+}
+
+void parseTransfersData(vector<list<int>>& adjacent, map<string, int>& moo, vector<string>& int_to_stop_id) {
+	ifstream stopFile("transfers.txt");
+	if (!stopFile) {
+		cerr << "Yo the transfers data file's missing.\n";
+		exit(1);
+	}
+	string line;
+	getline(stopFile, line);
+	while (getline(stopFile, line)) { //adjacent index represents init transfer point
+		string from = line.substr(0,line.find(','));
+		string to = line.substr(4,3);
+		cout << from << "|" << to << "|" << moo[from] << "|" << moo[to] << endl;
+
 	}
 }
 
@@ -27,5 +47,6 @@ int main() {
 	map<string, int> moo;
 	vector<string> int_to_stop_id;
 	parseTrainData(moo, int_to_stop_id);
-
+	vector<list<int>> adjacent;
+	parseTransfersData(adjacent, moo, int_to_stop_id);
 }
