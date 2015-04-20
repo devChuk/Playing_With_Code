368439cf3f63882f0a52e841946272a91ceae7da
Brian Chuk <brian_chuk@yahoo.com>
2015-04-20 17:59:12 -0400

    and were done here

--- C++/lolwat/hw07/hw07.cpp
+++ C++/lolwat/hw07/hw07.cpp
@@ -47,9 +47,16 @@ void parseTransfersData(vector<list<int>>& adjacent, map<string, int>& moo, vect
 			adjacent.push_back(*meow);//create new slot
 		}
 		else {
-			adjacent[adjacent.size() - 1].push_back(moo[to]);
+			adjacent[adjacent.size() - 1].push_front(moo[to]);
 		}
 	}
+	adjacent[0].push_front(1);
+	adjacent[adjacent.size() - 1].push_front(adjacent.size());
+	for (int i = 1; i < adjacent.size() - 1; i++) {
+		adjacent[i].push_front(i - 1);
+		adjacent[i].push_front(i + 1);
+	}
+
 }
 /*  TEST CODE
 int main() {
@@ -76,9 +83,7 @@ class BinaryNode {
     BinaryNode *right;
     int size;
     
-    BinaryNode( const Comparable & theElement, BinaryNode *lt,
-               BinaryNode *rt, int sz = 1 )
-    : element( theElement ), left( lt ), right( rt ), size( sz ) { }
+    BinaryNode(const Comparable & theElement, BinaryNode *lt, BinaryNode *rt, int sz = 1) : element(theElement), left(lt), right(rt), size(sz) {}
     
     friend class BinarySearchTree<Comparable>;
 };
@@ -88,34 +93,112 @@ class BinarySearchTree {
 public:
     typedef BinaryNode<Comparable> Node;
     
-    BinarySearchTree( ) : root( NULL ) { } // Construct the tree.
-    ~BinarySearchTree( ){ makeEmpty( ); } // Destructor for the tree.
+    BinarySearchTree() : root(NULL) {} // Construct the tree.
+    ~BinarySearchTree(){makeEmpty();} // Destructor for the tree.
     
 
     // add function declarations
     
     
-    bool isEmpty( ) const { return root == NULL;}
-    void makeEmpty( ) { makeEmpty( root ); }
-    void insert( const Comparable & x ) { insert( x, root ); }
-    
+    bool isEmpty() const {return root == NULL;}
+    void makeEmpty() {makeEmpty(root);}
+    void insert(const Comparable & x) {insert(x,root);}
+
+    //									_________________________________________A_________________________________________
+  	Node* find(const Comparable& x) {
+  		find(x, root);
+    }
+
+    Node* find(const Comparable& x, Node* root) {
+    	if (root == nullptr) return nullptr;
+    	if (x == root->element)
+    		return root;
+    	if (x > root->element)
+    		return find(x, root->right);
+    	else
+    		return find(x, root->left);
+    }
+
+    //									_________________________________________B_________________________________________
+    void range(const Comparable& low, const Comparable& high) {
+    	range(root, low, high);
+    }
+    void range(Node* root, const Comparable& low, const Comparable& high) {
+    	if (root == nullptr) cout << endl;
+    	else {
+    		if (root->element > low && root->element < high) cout << root->element;
+	    	range(root->left,low,high);
+	    	range(root->right,low,high);
+	    }
+	}
+
+    //									_________________________________________c_________________________________________
+	void negateTree() {
+		negateTree(root);
+	}
+	void negateTree(Node* root) {
+		if (root != nullptr) {
+			root->element = -1 * root->element;
+			Node* temp = root->left;
+			root->left = root->right;
+			root->right = temp;
+			negateTree(root->right);
+			negateTree(root->left);
+		}
+	}
+
+    //									_________________________________________D_________________________________________
+	int CountNodesWithOneChild() {
+		return CountNodesWithOneChild(root);
+	}
+	int CountNodesWithOneChild(Node* root) {
+		if (root == nullptr) return 0;
+		if (root->left != nullptr && root->right != nullptr)
+			return CountNodesWithOneChild(root->left) + CountNodesWithOneChild(root->right);
+		if (root->left != nullptr)
+			return 1 + CountNodesWithOneChild(root->left);
+		if (root->right != nullptr)
+			return 1 + CountNodesWithOneChild(root->right);
+		return 0;
+	}
+
+    //									_________________________________________E_________________________________________
+	int average_node_depth() {
+		vector<int> depths;
+		add_node_depth(depths, root, 0);
+		double avg;
+		for (int i = 0; i < depths.size(); i++)
+			avg += depths[i];
+		avg /= depths.size();
+		return avg;
+	}
+	void add_node_depth(vector<int> depths, Node* root, int depth) {
+		if (root != nullptr) {
+			depths.push_back(depth);
+			if (root->right != nullptr)
+				add_node_depth(depths,root->right,depth+1);
+			if (root->left != nullptr)
+				add_node_depth(depths,root->left,depth+1);
+		}
+	}
+	
 private:
     Node * root;
 
     // add function declarations
     
-    int treeSize( Node *t ) const { return t == NULL ? 0 : t->size; }
-    Comparable const * elementAt( Node *t ) const;
-    void insert( const Comparable & x, Node * & t );
-    void makeEmpty( Node * & t ) const;
-    Node * clone( Node *t ) const;
+    int treeSize(Node *t) const {return t == NULL ? 0 : t->size;}
+    Comparable const * elementAt(Node *t) const;
+    void insert(const Comparable & x, Node * & t);
+    void makeEmpty(Node * & t) const;
+    Node * clone(Node *t) const;
 };
 
 
 
 template <class Comparable>
-Comparable const * BinarySearchTree<Comparable>::elementAt( Node *t ) const {
-    if( t == NULL )
+Comparable const * BinarySearchTree<Comparable>::elementAt(Node *t) const {
+    if(t == NULL)
         return NULL;
     else
         return &(t->element);
@@ -148,5 +231,10 @@ void BinarySearchTree<Comparable>::makeEmpty( Node * & t ) const {
 }
 
 int main() {
-
+	BinarySearchTree<int> oak;
+	for (int i = 0; i < 10; i++) {
+		oak.insert(i);
+	}
+	// cout << oak.find(5)->element << endl; //this won't work actually unless the element variable is set as public
+	// oak.range(0,10);
 }
