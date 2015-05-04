da15554c3a100dfa000f6bb427469bc376b943f7
Brian Chuk <brian_chuk@yahoo.com>
2015-05-04 19:28:52 -0400

    problem 1, init problem 2

--- C++/lolwat/hw08/hw08.cpp
+++ C++/lolwat/hw08/hw08.cpp
@@ -1,9 +1,4 @@
 //Brian Chuk
-
-
-// Assumption: The vertices of the graph are labeled from 0 to n - 1,
-// where n is the number of vertices.
-
 #include <iostream>
 #include <fstream>
 #include <list>
@@ -13,6 +8,60 @@
 #include <algorithm>
 #include <map>
 using namespace std;
+
+//PROBLEM 1///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Assumption: The vertices of the graph are labeled from 0 to n - 1,
+// where n is the number of vertices.
+
+void parseTrainData(map<string, int>& moo, vector<string>& int_to_stop_id) { //fills up moo & int_to_stop_id
+  ifstream stopFile("MTA_train_stop_data.txt");
+  if (!stopFile) {
+    cerr << "Yo the train stop data file's missing.\n";
+    exit(1);
+  }
+  string line;
+  getline(stopFile, line);
+  int count = 0;
+  while (getline(stopFile, line)) {
+    int comma = line.find(',');
+    string stop_id = line.substr(0,comma);
+    if (stop_id.substr(comma-1) != "S" && stop_id.substr(comma-1) != "N") {
+      moo[stop_id] = count;
+      int_to_stop_id.push_back(stop_id);
+      count++;
+    }
+  }
+}
+
+void parseTransfersData(vector<list<int>>& adjacent, map<string, int>& moo, vector<string>& int_to_stop_id) { //fills up adjacent with lists of transferrable numbers
+  ifstream stopFile("transfers.txt");
+  if (!stopFile) {
+    cerr << "Yo the transfers data file's missing.\n";
+    exit(1);
+  }
+  string line;
+  getline(stopFile, line);
+  while (getline(stopFile, line)) { //adjacent index represents init transfer point
+    string from = line.substr(0,line.find(','));
+    string to = line.substr(4,3);
+    // cout << from << "|" << to << "|" << moo[from] << "|" << moo[to] << endl;
+    if (adjacent.size() == moo[from]) {
+      list<int>* meow = new list<int>;
+      meow->push_back(moo[to]);
+      adjacent.push_back(*meow);//create new slot
+    }
+    else {
+      adjacent[adjacent.size() - 1].push_front(moo[to]);
+    }
+  }
+  adjacent[0].push_front(1);
+  adjacent[adjacent.size() - 1].push_front(adjacent.size());
+  for (int i = 1; i < adjacent.size() - 1; i++) {
+    adjacent[i].push_front(i - 1);
+    adjacent[i].push_front(i + 1);
+  }
+}
 
 const int DEFAULT_VAL =  -1;      // must be less than 0
 
@@ -103,23 +152,53 @@ void shortestpaths( Graph & g, int s) {
 //            5*---*4
 //
 //
+// int main() {
+//   // Graph g(6);
+
+//   // g[0].push_back(2);
+//   // g[1].push_back(3);
+//   // g[1].push_back(2);
+//   // g[2].push_back(3);
+//   // g[2].push_back(5);
+//   // g[2].push_back(1);
+//   // g[2].push_back(0);
+//   // g[3].push_back(1);
+//   // g[3].push_back(2);
+//   // g[3].push_back(4);
+//   // g[5].push_back(4);
+//   // g[4].push_back(5);
+//   // g[4].push_back(3);
+//   // g[5].push_back(2);
+
+//   // shortestpaths(g,1);
+
+//   map<string, int> moo;
+//   vector<string> int_to_stop_id;
+//   parseTrainData(moo, int_to_stop_id);
+//   vector<list<int>> adjacent;
+//   parseTransfersData(adjacent, moo, int_to_stop_id);
+
+//   shortestpaths(adjacent,183);
+
+//   cout << adjacent.size() << endl;
+
+//   for (list<int>::iterator it=adjacent[183].begin(); it != adjacent[183].end(); ++it)
+//       cout << ' ' << *it;
+
+
+// }
+
+//PROBLEM 2///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int ASCIIhash(string input) {
+  int ans = 0;
+  for (unsigned i = 0; i < input.size(); i++)
+    ans += int(input.at(i));
+  return ans;
+}
+
+
+
 int main() {
-     Graph g(6);
-
-     g[0].push_back(2);
-     g[1].push_back(3);
-     g[1].push_back(2);
-     g[2].push_back(3);
-     g[2].push_back(5);
-     g[2].push_back(1);
-     g[2].push_back(0);
-     g[3].push_back(1);
-     g[3].push_back(2);
-     g[3].push_back(4);
-     g[5].push_back(4);
-     g[4].push_back(5);
-     g[4].push_back(3);
-     g[5].push_back(2);
-
-     shortestpaths(g,1);
+  // cout << ASCIIhash("what") << endl;
 }
