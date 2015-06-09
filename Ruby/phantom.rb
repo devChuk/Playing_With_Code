22fa58ca937a6816e7a1b663dfaa1687c48c3060
Brian Chuk <brian_chuk@yahoo.com>
2015-06-09 00:20:08 -0400

    setup

--- Ruby/phantom.rb
+++ Ruby/phantom.rb
@@ -1,2 +1,10 @@
 # A tester for [redacted]
 
+require 'page-object'
+require 'selenium-webdriver'
+
+@browser = Selenium::WebDriver.for $webdriver
+@browser.manage.timeouts.implicit_wait = 10
+# @browser.manage.window.resize_to(1000, 800) 1124, 850
+@browser.manage.window.resize_to(1124, 850)
+@super_admin = Admin.new(@browser)
