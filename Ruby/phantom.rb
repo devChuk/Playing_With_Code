26c6b0295ceee1b723c8058149b53d985124fd84
Brian Chuk <brian_chuk@yahoo.com>
2015-06-09 11:46:32 -0400

    confusion

--- Ruby/phantom.rb
+++ Ruby/phantom.rb
@@ -1,10 +1,24 @@
 # A tester for [redacted]
-
 require 'page-object'
 require 'selenium-webdriver'
+require 'rubygems'
 
-@browser = Selenium::WebDriver.for $webdriver
+class Login
+	include PageObject
+end
+
+@browser = Selenium::WebDriver.for :phantomjs
 @browser.manage.timeouts.implicit_wait = 10
 # @browser.manage.window.resize_to(1000, 800) 1124, 850
 @browser.manage.window.resize_to(1124, 850)
-@super_admin = Admin.new(@browser)
+
+@browser.get "http://qa.betterpath.com"
+hurr = Login.new(@browser)
+
+
+@browser.find_element(:name => 'email').text
+hurr.wait_until do
+    @browser.find_element(:name => 'email').text == 'Forgot password?'
+end
+
+# @browser.save_screenshot("./blah.png")
