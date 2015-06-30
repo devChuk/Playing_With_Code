b428d78ed67f4063db05355ef10475b1e6df61f7
Brian Chuk <brian_chuk@yahoo.com>
2015-06-30 11:15:05 -0400

    threeJS experiments

---
+++ HTML/THREEjs/animation experiments/devchuk2/res/js/index.js
@@ -0,0 +1,117 @@
+/*
+Title:			BrianCh.uk Home Screen Javascript v1
+Author:			Brian Chuk (brianch.uk)
+Copyright: 		2015, Brian Chuk - All rights reserved (you have permission to use it, just tell me :D)
+*/
+var mobile = false;
+	if (window.innerWidth <= 767) {
+		mobile = true;
+	}
+
+var animated = true;
+if (window.innerWidth > 991) {
+	var hack_the_city = document.createElement('script');
+	hack_the_city.src = './res/js/temp.js';
+	hack_the_city.type = 'text/javascript';
+	document.body.appendChild(hack_the_city);
+}
+else {animated = false;}
+
+$( window ).resize(function() {
+	
+	if (!animated && window.innerWidth > 991) {
+		var hack_the_city = document.createElement('script');
+		hack_the_city.src = './res/js/temp.js';
+		hack_the_city.type = 'text/javascript';
+		document.body.appendChild(hack_the_city);
+		animated = true;
+	}
+
+	if (window.innerWidth <= 767) {
+		mobile = true;
+		$(".profile").hide();
+	}
+	else {
+		mobile = false;
+		$("#profile").show();
+	}
+
+});
+
+var current = "#profile";
+
+$("#detail1").click(function () {
+	if (!mobile)
+		$( current ).finish().slideUp( 50, function() {
+		    if ("#profile1" == current) {
+		    	$( "#profile" ).finish().slideDown("fast");
+		    	current = "#profile";
+		    }
+		    else {
+			    $( "#profile1" ).finish().slideDown("fast");
+			    current = "#profile1";
+			}
+		});
+	else
+		document.location = "http://www.nyu.edu/";
+});
+
+$("#detail2").click(function () {
+	if (!mobile)
+		$( current ).finish().slideUp( 50, function() {
+	    	if ("#profile2" == current) {
+		    	$( "#profile" ).finish().slideDown("fast");
+		    	current = "#profile";
+		    }
+		    else {
+			    $( "#profile2" ).finish().slideDown("fast");
+			    current = "#profile2";
+			}
+	    });
+	else document.location = "http://www.syncano.com";
+});
+
+$("#detail3").click(function () {
+	if (!mobile)
+		$( current ).finish().slideUp( 50, function() {
+	    	if ("#profile3" == current) {
+		    	$( "#profile" ).finish().slideDown("fast");
+		    	current = "#profile";
+		    }
+		    else {
+			    $( "#profile3" ).finish().slideDown("fast");
+			    current = "#profile3";
+			}
+	    });
+	else document.location = "http://hackny.org/a/fellows/";
+});
+
+$("#detail4").click(function () {
+	if (!mobile)
+		$( current ).finish().slideUp( 50, function() {
+	    	if ("#profile4" == current) {
+		    	$( "#profile" ).finish().slideDown("fast");
+		    	current = "#profile";
+		    }
+		    else {
+			    $( "#profile4" ).finish().slideDown("fast");
+			    current = "#profile4";
+			}
+	    });
+	else document.location = "http://betterpath.com";
+});
+
+$("#detail5").click(function () {
+	if (!mobile)
+		$( current ).finish().slideUp( 50, function() {
+	    	if ("#profile5" == current) {
+		    	$( "#profile" ).finish().slideDown("fast");
+		    	current = "#profile";
+		    }
+		    else {
+			    $( "#profile5" ).finish().slideDown("fast");
+			    current = "#profile5";
+			}
+	    });
+	else document.location = "http://kiteandrocket.com";
+});
---
+++ HTML/THREEjs/animation experiments/devchuk2/res/js/temp.js
@@ -0,0 +1,131 @@
+/*
+Title:			BrianCh.uk Home Screen Javascript ThreeJS Animation v1
+Author:			Brian Chuk (brianch.uk)
+Copyright: 		2015, Brian Chuk - All rights reserved (you have permission to use it, just tell me :D)
+
+I would use some of my other animations for the background, but I've found that it was too distracting haha.
+Test users would just stare at the animation & not realize that they could scroll down haha
+
+*/
+
+var container;
+
+var camera, scene, renderer;
+
+var geometry, group;
+
+var mouseX = 0, mouseY = 0;
+
+var windowHalfX = window.innerWidth / 2;
+var windowHalfY = window.innerHeight / 2;
+
+document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+
+init();
+animate();
+
+function init() {
+
+	container = document.createElement( 'div' );
+	var $containerz = $('#canvas');
+	$containerz.append(container);
+
+	camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 10000 );
+	camera.position.z = 500;
+
+	scene = new THREE.Scene();
+
+	var geometry = new THREE.BoxGeometry( 100, 100, 100 );
+
+	group = new THREE.Group();
+
+	var colors = ['0xF44336', '0x9C27B0', '0x2196F3', '0x4CAF50', '0xFFC107', '0xFF5722'];
+
+	for ( var i = 0; i < 1000; i ++ ) {
+
+		var pls = colors[Math.floor(Math.random() * 6)];
+
+		var material = new THREE.MeshBasicMaterial();
+
+		var mesh = new THREE.Mesh( geometry, material );
+		mesh.position.x = Math.random() * 2000 - 1000;
+		mesh.position.y = Math.random() * 2000 - 1000;
+		mesh.position.z = Math.random() * 2000 - 1000;
+
+		mesh.rotation.x = Math.random() * 2 * Math.PI;
+		mesh.rotation.y = Math.random() * 2 * Math.PI;
+
+		mesh.material.color.setHex( pls );
+
+		mesh.matrixAutoUpdate = false;
+		mesh.updateMatrix();
+
+		group.add( mesh );
+
+	}
+
+	scene.add( group );
+
+	renderer = new THREE.WebGLRenderer();
+	renderer.setClearColor( "#2980B9" );
+	renderer.setPixelRatio( window.devicePixelRatio );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.sortObjects = false;
+
+	container.appendChild( renderer.domElement );
+
+//
+
+window.addEventListener( 'resize', onWindowResize, false );
+
+}
+
+function onWindowResize() {
+
+	windowHalfX = window.innerWidth / 2;
+	windowHalfY = window.innerHeight / 2;
+
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+function onDocumentMouseMove(event) {
+
+	mouseX = ( event.clientX - windowHalfX ) * 10;
+	mouseY = ( event.clientY - windowHalfY ) * 10;
+
+}
+
+//
+
+function animate() {
+
+	requestAnimationFrame( animate );
+
+	render();
+
+}
+
+function render() {
+
+	var time = Date.now() * 0.001;
+
+	var rx = Math.sin( time * 0.7 ) * 0.5,
+		ry = Math.sin( time * 0.3 ) * 0.5,
+		rz = Math.sin( time * 0.2 ) * 0.5;
+
+	// camera.position.x += ( mouseX - camera.position.x ) * .05;
+	// camera.position.y += ( - mouseY - camera.position.y ) * .05;
+
+	camera.lookAt( scene.position );
+
+	group.rotation.x = rx;
+	group.rotation.y = ry;
+	group.rotation.z = rz;
+
+	renderer.render( scene, camera );
+
+}
