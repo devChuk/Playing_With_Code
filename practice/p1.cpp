7c5e88ddef2169da960ef2ec13eabfcbe2180565
root <brian_chuk@yahoo.com>
2015-10-31 17:57:50 -0400

    just pushin something I forgot to commit before

--- practice/p1.cpp
+++ practice/p1.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
 #include <string>
+#include <vector>
 	using namespace std;
 
-int main() {
+void bacon() {
+	vector<int> okay;
 	cout << "p" << endl;
+	for (int i = 0; i <= 10; i++) {
+		okay.push_back(i);
+	}
+	int v = 0;
+	while (v < 10) {
+		cout << okay[v] << endl;
+		v++;
+	}
+}
+
+				return "false";
+			if (word[i] == word[j]) {
+			}
+		for (int j = i + 1; j < word.length(); j++) {
+		}
+	for (int i = 0; i < word.length(); i++) {
+	return "true";
+	}
+string checkit(string word) {
+}
+
+int main() {
+	cout << "abcde"[2] << endl;
+	cout << checkit("abccdefg") << endl;
 }
