54b6517bf85945d7f566ea44786574b87482c1ea
root <brian_chuk@yahoo.com>
2015-11-30 23:39:58 -0500

    I had a whole month to prepare but of course I waited til the day before my interview :) Well see what happens

---
+++ practice/CtCI/test.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstdlib>
+	using namespace std;
+
+// bool isDistinct(string s) {
+// 	for (int i = 0; i < s.length(); i++) {
+// 		for (int j = i + 1; j < s.length(); j++) {
+// 			if (s[i] == s[j]) {
+// 				return false;
+// 			}
+// 		}
+// 	}
+// 	return true;
+// }
+
+
+// int charArray [256];
+// 	for (int i = 0; i < sizeof(charArray)/4; i++) {
+// 		charArray[i] = 0;
+// 		cout << charArray[i] << endl;
+// 	}
+
+// string compress(string s) {
+// 	string ans = "";
+// 	int index = 0;
+// 	while (index < s.length()) {
+// 		char letter = s.at(index);
+// 		int count = 0;
+// 		while (index < s.length() && s.at(index) == letter) {
+// 			count++;
+// 			index++;
+// 		}
+// 		ans += letter;
+// 		ans += to_string(count);
+// 	}
+// 	return ans;
+// }
+
+// void printMatrix(vector<vector<int>> &matrix) {
+// 	for (int i = 0; i < matrix.size(); i++) {
+// 		for (int j = 0; j < matrix.size(); j++) {
+// 			cout << matrix[i][j] << " ";
+// 		}
+// 		cout << endl;
+// 	}
+// }
+
+// void FourNMatrix(vector<vector<int>> &matrix) {
+// 	for (int i = 0; i < 4; i++) {
+// 		vector<int> test;
+// 		for (int i = 0; i < 4; i++) {
+// 			test.push_back(rand() % 10);
+// 		}
+// 		matrix.push_back(test);
+// 	}
+// }
+
+// struct matrixPointer {
+// 	int x;
+// 	int y;
+// };
+
+// void rotate(vector<vector<int>> &matrix) {
+// 	int count = 0;
+// 	while (count <= matrix.size() / 2) {
+// 		matrixPointer topLeft, topRight, botLeft, botRight;
+// 		topLeft.x = count; topLeft.y = count;
+// 		botRight.x = matrix.size() - 1 - count; botRight.y = botRight.x;
+
+// 		count++;
+// 	}
+// }
+
+//vector<vector<int>> matrix;
+
+int main () {
+	
+}
