d7831387cecaaaf242b4aed8906b3dcf5dd02baf
root <brian_chuk@yahoo.com>
2016-06-15 19:04:06 -0700

    the long shot

---
+++ HTML/THREEjs/the_long_shot/res/js/index.js
@@ -0,0 +1,29 @@
+/*
+Title:			BrianCh.uk Home Screen Javascript v1
+Author:			Brian Chuk (brianch.uk)
+Copyright: 		2015, Brian Chuk - All rights reserved (you have permission to use it, just tell me :D)
+*/
+var mobile = false;
+	if (window.innerWidth <= 767) {
+		mobile = true;
+	}
+
+var animated = true;
+if (window.innerWidth > 991) {
+	var hack_the_city = document.createElement('script');
+	hack_the_city.src = './res/js/temp.js';
+	hack_the_city.type = 'text/javascript';
+	document.body.appendChild(hack_the_city);
+}
+else {animated = false;}
+
+$( window ).resize(function() {
+	
+	if (!animated && window.innerWidth > 991) {
+		var hack_the_city = document.createElement('script');
+		hack_the_city.src = './res/js/temp.js';
+		hack_the_city.type = 'text/javascript';
+		document.body.appendChild(hack_the_city);
+		animated = true;
+	}
+});
---
+++ HTML/THREEjs/the_long_shot/res/js/temp.js
@@ -0,0 +1,68 @@
+/*
+Title:			BrianCh.uk Home Screen Javascript ThreeJS Animation v1
+Author:			Brian Chuk (brianch.uk)
+Copyright: 		2015, Brian Chuk - All rights reserved (you have permission to use it, just tell me :D)
+
+I would use some of my other animations for the background, but I've found that it was too distracting haha.
+Test users would just stare at the animation & not realize that they could scroll down haha
+
+*/
+
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+// var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+// var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+// var cube = new THREE.Mesh( geometry, material );
+// scene.add( cube );
+
+// create the particle variables
+var particleCount = 1800,
+    particles = new THREE.Geometry(),
+    pMaterial = new THREE.ParticleBasicMaterial({
+      color: 0xFFFFFF,
+      size: 20
+    });
+
+// now create the individual particles
+for (var p = 0; p < particleCount; p++) {
+
+  // create a particle with random
+  // position values, -250 -> 250
+  var pX = Math.random() * 500 - 250,
+      pY = Math.random() * 500 - 250,
+      pZ = Math.random() * 500 - 250,
+      particle = new THREE.Vertex(
+        new THREE.Vector3(pX, pY, pZ)
+      );
+
+  // add it to the geometry
+  particles.vertices.push(particle);
+}
+
+// create the particle system
+var particleSystem = new THREE.ParticleSystem(
+    particles,
+    pMaterial);
+
+// add it to the scene
+scene.addChild(particleSystem);
+
+
+camera.position.z = 5;
+
+
+var render = function () {
+	requestAnimationFrame( render );
+
+	cube.rotation.x += 0.1;
+	cube.rotation.y += 0.1;
+
+	renderer.render(scene, camera);
+};
+
+render();
