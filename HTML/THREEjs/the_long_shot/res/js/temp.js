4608be5b9ce59324d584970626e474f372d6eaac
root <brian_chuk@yahoo.com>
2016-06-17 23:16:48 -0700

    brownian motion

--- HTML/THREEjs/the_long_shot/res/js/temp.js
+++ HTML/THREEjs/the_long_shot/res/js/temp.js
@@ -23,24 +23,56 @@ function onWindowResize(){
 
 }
 
+camera.position.z = 5;
+
 ////////////////////////////////////////CREATING OBJECTS////////////////////////////////////////
 
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+// var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+// var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+// var cube = new THREE.Mesh( geometry, material );
+// scene.add( cube );
+
+var material = new THREE.PointsMaterial({
+	color: 0xffffcc,
+	size: 1,
+	vertexColors: THREE.VertexColors
+});
+
+var geometry = new THREE.Geometry();
+var x, y, z;
+for (var i = 0; i < 1000; i++) {
+	x = (Math.random() * 800) - 400;
+	y = (Math.random() * 800) - 400;
+  	z = (Math.random() * 800) - 400;
+  	geometry.vertices.push(new THREE.Vector3(x, y, z));
+	geometry.colors.push(new THREE.Color(Math.random(), Math.random(), Math.random()));
+}
+
+var points = new THREE.Points(geometry, material);
+scene.add(points);
 
 
-
-camera.position.z = 5;
-
+////////////////////////////////////////RENDERING&&ANIMATING////////////////////////////////////////
 
 var render = function () {
-	requestAnimationFrame( render );
+	window.requestAnimationFrame( render );
 
-	cube.rotation.x += 0.1;
-	cube.rotation.y += 0.1;
+	geometry.vertices.forEach(function(point, index, array) {
+		var dX, dY, dZ;
+	    dX = Math.random() * 2 - 1;
+	    dY = Math.random() * 2 - 1;
+	    dZ = Math.random() * 2 - 1;
+
+	    geometry.colors[index] = new THREE.Color(Math.random(), Math.random(), Math.random());
+	    point.add(new THREE.Vector3(dX, dY, dZ));
+	    
+	    // console.log(index);
+	});
+
+
+	geometry.verticesNeedUpdate = true;
+	geometry.colorsNeedUpdate = true;
 
 	renderer.render(scene, camera);
 };
@@ -56,6 +88,13 @@ https://github.com/mrdoob/three.js/issues/1591
 
 // TODO
 // Make a particle cloud
+/*
+So they're called point clouds
+
+
+
+
+*/
 // ANIMATE PARTICLES IN OPENING MOTION
 // SEE IF I CAN MOVE THEM INTO A 3D MODELLED cube
 // Consider somehow animating this cube for science.
