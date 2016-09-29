6368e79c3bf17a78fa632f6b2fb274f357292bbe
root <brian_chuk@yahoo.com>
2016-09-29 05:28:21 -0400

    added folder for test code

---
+++ CTCI/testenv/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+
+#include <set>
+#include <functional>
+#include <algorithm>
+#include <math.h>
+
+int main() {
+	std::string s(5, 'a');
+	std::cout << s << std::endl;
+}
