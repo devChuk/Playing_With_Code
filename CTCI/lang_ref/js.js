5e73fe5148f4084bb855fa0347d3f9eb2989a9e1
root <brian_chuk@yahoo.com>
2016-09-29 05:50:29 -0400

    added js sheet

---
+++ CTCI/lang_ref/js.js
@@ -0,0 +1,35 @@
+function myFunction(p1, p2) {
+    return p1 * p2;              // The function returns the product of p1 and p2
+}
+
+for (i = 0; i < cars.length; i++) { 
+    text += cars[i] + "<br>";
+}
+
+var person = {fname:"John", lname:"Doe", age:25}; 
+
+var text = "";
+var x;
+for (x in person) {
+    text += person[x];
+}
+
+while (i < 10) {
+    text += "The number is " + i;
+    i++;
+}
+
+do {
+    text += "The number is " + i;
+    i++;
+}
+while (i < 10);
+
+console.log("lol");
+
+
+var length = 16;                               // Number
+var lastName = "Johnson";                      // String
+var cars = ["Saab", "Volvo", "BMW"];           // Array
+var x = {firstName:"John", lastName:"Doe"};    // Object
+var ay = true;								   // Bool
