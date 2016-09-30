7607851118b4d1acf05272f9ac5fdaa87a495de7
root <brian_chuk@yahoo.com>
2016-09-30 00:18:48 -0400

    init

---
+++ CTCI/lang_ref/Java.java
@@ -0,0 +1,38 @@
+public class HelloWorld
+{
+	
+
+	public static int methodName(int a, int b) {
+   		// body
+	}
+
+	for(int x = 10; x < 20; x = x + 1) {
+         System.out.print("value of x : " + x );
+     }
+
+     while (false) {
+
+     }
+
+     do {
+         System.out.print("value of x : " + x );
+         x++;
+         System.out.print("\n");
+      }while( x < 20 );
+
+      break;
+
+
+	for( String name : names ) {
+         System.out.print( name );
+         System.out.print(",");
+      }		
+
+
+
+
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
