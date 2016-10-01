9c9e420b0712a0dda64b6d22975325627171fae0
root <brian_chuk@yahoo.com>
2016-10-01 00:19:28 -0400

    js

--- CTCI/lang_ref/js.js
+++ CTCI/lang_ref/js.js
@@ -25,6 +25,7 @@ do {
 }
 while (i < 10);
 
+break;
 console.log("lol");
 
 
@@ -33,3 +34,77 @@ var lastName = "Johnson";                      // String
 var cars = ["Saab", "Volvo", "BMW"];           // Array
 var x = {firstName:"John", lastName:"Doe"};    // Object
 var ay = true;								   // Bool
+var me = undefined;
+
+var txt = "ay";
+txt.length;
+txt.indexOf("a");
+txt.lastIndexOf("a");
+
+
+var str = "Apple, Banana, Kiwi";
+var res = str.slice(7,13);
+var res = str.substring(7,13);
+
+str = "Please visit Microsoft!";
+var n = str.replace("Microsoft","W3Schools");
+
+var text1 = "Hello World!";       // String
+var text2 = text1.toUpperCase();  // text2 is text1 converted to upper
+text1.toLowerCase();
+
+var text = "Hello" + " " + "World!";
+var text = "Hello".concat(" ","World!");
+
+var str = "HELLO WORLD";
+str.charAt(0);            // returns H
+str.charCodeAt(0);         //	returns 72
+
+var txt = "a,b,c,d,e";   // String
+txt.split(",");          // Split on commas
+
+
+var fruits = [];
+fruits.toString();
+fruits.join(" * ");
+fruits.pop(); fruits.push();
+fruits.shift(); fruits.unshift();
+fruits[0] = "Kiwi";
+fruits.splice(2, 0, "Lemon", "Kiwi");
+var myChildren = myGirls.concat(myBoys);
+var citrus = fruits.slice(3);
+
+fruits.sort();				//alphabetical only, like strings
+fruits.reverse();
+points.sort(function(a, b){return a - b});
+
+
+
+
+switch (new Date().getDay()) {
+    case 0:
+        day = "Sunday";
+        break;
+    case 1:
+        day = "Monday";
+        break;
+    case 2:
+        day = "Tuesday";
+        break;
+    case 3:
+        day = "Wednesday";
+        break;
+    case 4:
+        day = "Thursday";
+        break;
+    case 5:
+        day = "Friday";
+        break;
+    case 6:
+        day = "Saturday";
+}
+
+
+person.lastName;
+person["lastName"];
+
