d5a3054325c7b80e5fca52cfe2f35cbb88bdbadf
root <brian_chuk@yahoo.com>
2016-10-09 02:07:01 -0400

    init 1000

---
+++ CTCI/solutions/arraysAndStrings.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <set>
+#include <functional>
+#include <algorithm>
+#include <math.h>
+
+int main() {
+	std::cout << "ay" << std::endl;
+
+	return 0;
+}
