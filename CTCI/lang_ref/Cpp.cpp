b7c06da45e1a8ca2b19317123dc58ce8e86a7bd5
root <brian_chuk@yahoo.com>
2016-10-13 16:25:04 -0400

    hm

--- CTCI/lang_ref/Cpp.cpp
+++ CTCI/lang_ref/Cpp.cpp
@@ -18,6 +18,102 @@ int runFunction(int one, int two) {
 	return one;
 }
 
+bool uniqueChars(std::string input) {
+	// std::vector<std::string> chars;
+	// for (int i = 0; i < input.length; i++) {
+	// 	if (chars.indexOf(input.str(i,i+1))  == -1)
+	// 		chars.push_back(input.str(i,i+1));
+	// 	else
+	// 		return false;
+	// 	return true;
+	// }
+	//////////
+	// std::vector<std::string> chars;
+	// for (int i = 0; i < input.length(); i++) {
+	// 	if (std::find(chars.begin(),chars.end(),input.substr(i,i+1))  == chars.end())
+	// 		chars.push_back(input.substr(i,i+1));
+	// 	else
+	// 		return false;
+	// }
+	// return true;
+	///////////
+	for (int i = 0; i < input.length(); i++) {
+		for (int j = i + 1; j < input.length(); j++) {
+			if (input.substr(i,i+1) == input.substr(j,j+1))
+				return false;
+		}
+	}
+	return true;
+}
+
+void reverse(char* str) {
+	// char* end = str;
+	// char* ans;
+	// if (*str) {
+	// 	while (*end)
+	// 		end++;
+	// 	while (*end != *str) {
+	// 		ans = end;
+	// 		ans++;
+	// 		*end--;
+	// 	}
+	// }
+	// return ans;
+	////////////
+	char* end = str;
+	char tmp;
+	if (*str) {
+		while (*end)
+			end++;
+		end--;
+		while (end > str) {
+			tmp = *str;
+			*str++ = *end;
+			*end-- = tmp;
+		}
+	}
+}
+
+bool permuCheck(std::string strA, std::string strB) {
+	if (strA.length() == strB.length()) {
+		std::sort(strA.begin(), strA.end());
+		std::sort(strB.begin(), strB.end());
+		for (int i = 0; i < strA.length(); i++)
+			if (strA[i] != strB[i])
+				return false;
+		return true;
+	} else
+	return false;
+	
+	// OR, MAKE AN ARRAY THAT RECORDS THE FREQUENCY OF ALL POSSIBLE CHARS
+	// THEN HAVE IT RECORD THE FREQ OF CHARS IN strA, then remove/compare with strB.
+}
+
+
+// "Mr|John|||Smith|"
+void replaceTwenty(std::string& str, int length) {
+	int count = 0, newLength;
+
+	for (int i = 0; i < length; i++)
+		if (str[i] == ' ')
+			count++;
+
+	newLength = length + 2 * count;
+	str[newLength] = '\0';
+
+	for (int i = length - 1; i >= 0; i--) {
+		if (str[i] == ' ') {
+			str[newLength - 1] = '0';
+			str[newLength - 2] = '2';
+			str[newLength - 3] = '%';
+			newLength = newLength -3;
+		} else {
+			str[newLength - 1] = str[i];
+			newLength--;
+		}
+	}
+}
+
 int main() {
 	
 	while(false) {
@@ -108,6 +204,12 @@ int main() {
 
 
 	runFunction(1,2);
+
+	// char somebuffer[100] = "a string";
+	// char* ptr = somebuffer;  // ptr now points to somebuffer
+	// std::cout << ptr << std::endl; // prints "a string"
+	// reverse(ptr);
+	// std::cout << ptr << std::endl; // prints "a string"
 	
 	/*
 	// 3
