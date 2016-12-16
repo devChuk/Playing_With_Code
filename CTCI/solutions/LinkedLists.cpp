5337f18d967226c4b1c5989235efacc2a111d7ec
root <brian_chuk@yahoo.com>
2016-12-15 23:52:00 -0500

    practicing

--- CTCI/solutions/LinkedLists.cpp
+++ CTCI/solutions/LinkedLists.cpp
@@ -6,6 +6,7 @@
 #include <algorithm>
 #include <math.h>
 #include <unordered_map>
+#include <stack>
 
 struct LLNode {
 	LLNode(int newData, LLNode* newNext) {
@@ -37,6 +38,32 @@ void removeDupes(LLNode* head) {
 		}
 }
 
+bool isPalindrome(LLNode* head) {
+	LLNode* slow = head;
+	LLNode* fast = head;
+	std::stack<int> stack;
+
+	while (fast != NULL && fast->next != NULL) {
+		stack.push(slow->data);
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+
+	if (fast != NULL) {
+		slow = slow->next;
+	}
+
+	while (slow != NULL) {
+		if (slow->data == stack.top()) {
+			stack.pop();
+			slow = slow->next;
+		} else {
+			return false;
+		}
+	}
+	return true;
+}
+
 int main() {
 	// LLNode* head = new LLNode(1);
 	// LLNode* second = new LLNode(2);
@@ -57,17 +84,16 @@ int main() {
 	// seventh->next = eighth;
 	// eighth->next = ninth;
 
-
 	LLNode* head = new LLNode(1);
 	LLNode* second = new LLNode(2);
 	LLNode* third = new LLNode(3);
 	LLNode* fourth = new LLNode(4);
 	LLNode* fourthTWO = new LLNode(4);
-	LLNode* fifth = new LLNode(5);
-	LLNode* sixth = new LLNode(6);
-	LLNode* seventh = new LLNode(7);
-	LLNode* eighth = new LLNode(8);
-	LLNode* ninth = new LLNode(9);
+	LLNode* fifth = new LLNode(3);
+	LLNode* sixth = new LLNode(2);
+	LLNode* seventh = new LLNode(1);
+	// LLNode* eighth = new LLNode(8);
+	// LLNode* ninth = new LLNode(9);
 
 	head->next = second;
 	second->next = third;
@@ -76,9 +102,8 @@ int main() {
 	fourthTWO->next = fifth;
 	fifth->next = sixth;
 	sixth->next = seventh;
-	seventh->next = eighth;
-	eighth->next = ninth;
-
+	// seventh->next = eighth;
+	// eighth->next = ninth;
 
 	LLNode* runner = head;
 	while (runner != NULL) {
@@ -87,7 +112,8 @@ int main() {
 	}
 	std::cout << std::endl;
 
-	removeDupes(head);
+	// insert function here to test.
+	std::cout << isPalindrome(head) << std::endl;
 
 
 	runner = head;
