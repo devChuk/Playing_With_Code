9af69f695b1041e2d0ef845ed71153687408a195
root <brian_chuk@yahoo.com>
2016-12-16 23:29:33 -0500

    a

---
+++ CTCI/lang_ref/completeCPPref.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+
+#include <set>
+#include <functional>
+#include <algorithm>
+#include <math.h>
+
+
+//Full Mastery
+
+// Arrays
+// Vectors
+// Strings
+// LinkedLists
+// Stacks
+// Queues
+// Trees
+// Graphs
+// Hashmaps (unordered_map)
+// Heaps (map)
+
+
+//I'll make a compact cheatsheet on Illustrator later. Here's what to add to it:
+// http://bigocheatsheet.com/
+// the chart, common data structure operation runtimes
+// how to use them in C++
+// magic STD functions
