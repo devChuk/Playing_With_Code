e8c525b41b2e70ad5d5dee8b280e6d09441a47be
root <brian_chuk@yahoo.com>
2016-12-19 00:43:02 -0500

    notes

--- CTCI/solutions/TreesandGraphs.cpp
+++ CTCI/solutions/TreesandGraphs.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <stdlib.h>
 
 
 #include <set>
@@ -8,14 +9,46 @@
 #include <algorithm>
 #include <math.h>
 
-struct TreeNode {
-	TreeNode* left;
-	TreeNode* right;
+struct TNode {
+	TNode* left;
+	TNode* right;
 	int data;
 };
 
+struct GNode {
+	int data;
+	std::vector<GNode*> nodes;
+};
 
+int checkHeight(TNode* head) {
+	if (!head)
+		return 0;
+
+	int leftHeight = checkHeight(head->left);
+	if (leftHeight == -1)
+		return -1;
+
+	int rightHeight = checkHeight(head->right);
+	if (rightHeight == -1)
+		return -1;
+
+	if (abs(leftHeight - rightHeight) > 1)
+		return -1;
+	else {
+		return std::max(leftHeight, rightHeight) + 1;
+	}
+}
+
+bool isBalanced(TNode* head) {
+	if (checkHeight(head) == -1)
+		return false;
+	else {
+		return true;
+	}
+}
 
 int main() {
-
+	if (-1) {
+		std::cout << "test" << std::endl;
+	}
 }
