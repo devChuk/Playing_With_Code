6c278535db3d7106476f2dce6762c1217b43a1bb
root <brian_chuk@yahoo.com>
2016-12-18 21:31:12 -0500

    init

---
+++ CTCI/solutions/TreesandGraphs.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+
+#include <set>
+#include <functional>
+#include <algorithm>
+#include <math.h>
+
+struct TreeNode {
+	TreeNode* left;
+	TreeNode* right;
+	int data;
+};
+
+
+
+int main() {
+
+}
