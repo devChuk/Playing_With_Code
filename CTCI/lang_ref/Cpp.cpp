b691f99165e5b5e1bee4fa70080a39ea6465986c
root <brian_chuk@yahoo.com>
2016-12-20 03:37:33 -0500

    done practicing for the day. I need to study for finals, do my interview, and start my final project lol

--- CTCI/lang_ref/Cpp.cpp
+++ CTCI/lang_ref/Cpp.cpp
@@ -8,6 +8,8 @@
 #include <algorithm>
 #include <math.h>
 
+#include <fstream>
+
 template <class Object>
 Object sumVector(const std::vector<Object> & a, int left, int right) {
         
@@ -224,7 +226,64 @@ int main() {
         cin >> a >> b;
         cout << a+b << "\n";
     }
-    */   
+    */
+
+
+    /*
+   char data[100];
+
+   // open a file in write mode.
+   ofstream outfile;
+   outfile.open("afile.dat");
+
+   cout << "Writing to the file" << endl;
+   cout << "Enter your name: "; 
+   cin.getline(data, 100);
+
+   // write inputted data into the file.
+   outfile << data << endl;
+
+   cout << "Enter your age: "; 
+   cin >> data;
+   cin.ignore();
+   
+   // again write inputted data into the file.
+   outfile << data << endl;
+
+   // close the opened file.
+   outfile.close();
+
+   // open a file in read mode.
+   ifstream infile; 
+   infile.open("afile.dat"); 
+ 
+   cout << "Reading from the file" << endl; 
+   infile >> data; 
+
+   // write the data at the screen.
+   cout << data << endl;
+   
+   // again read the data from the file and display it.
+   infile >> data; 
+   cout << data << endl; 
+
+   // close the opened file.
+   infile.close();
+
+	*/
+
+   /*
+	std::string line;
+	while (std::getline(infile, line))
+	{
+		std::istringstream iss(line);
+		int a, b;
+		if (!(iss >> a >> b)) { break; } // error
+
+	// process pair (a,b)
+	}
+
+	*/
 
     return 0;
 }
--- CTCI/solutions/TreesandGraphs.cpp
+++ CTCI/solutions/TreesandGraphs.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <vector>
+#include <list>
 #include <string>
 #include <stdlib.h>
+#include <limits>
 
 
 #include <set>
@@ -12,6 +14,10 @@
 #include <queue>
 
 struct TNode {
+
+	TNode(int item) {
+		data = item;
+	}
 	TNode* left;
 	TNode* right;
 	int data;
@@ -94,8 +100,130 @@ bool checkForRoute(GNode* start, GNode* end) {
 	return false;
 }
 
-void hurrdurr(int swag[]) {}
+TNode* genMinialHeightBST(std::vector<int> array) {
+	int n = array.size();
+
+	if (n == 0) {
+		return NULL;
+	} else if (n == 1) {
+		TNode* leaf = new TNode(array[0]);
+		return leaf;
+	} else {
+		TNode* head = new TNode(array[n/2]);
+		std::vector<int> leftHalf(array.begin(), array.begin() + n/2);
+		std::vector<int> rightHalf(array.begin() + n/2 + 1, array.end());
+		head->left = genMinialHeightBST(leftHalf);
+		head->right = genMinialHeightBST(rightHalf);
+		return head;
+	}
+}
+
+void printDFSTree(TNode* root) {
+	if (root) {
+		std::cout << root->data << std::endl;
+		printDFSTree(root->left);
+		printDFSTree(root->right);
+	}
+}
+
+void printBFSTree(TNode* root) {
+	std::queue<TNode*> queue;
+	queue.push(root);
+
+	while (!queue.empty()) {
+		TNode* r = queue.front();
+		queue.pop();
+
+		if (r != NULL) {
+			std::cout << r->data << std::endl;
+			queue.push(r->left);
+			queue.push(r->right);
+		}
+	}
+}
+
+void addToLists(TNode* root, std::vector<std::list<int>> lists, int depth) {
+	if (root != NULL) {
+		if (lists.size() <= depth) {
+			std::list<int> list;
+			lists.push_back(list);
+		}
+		lists[depth].push_back(root->data);
+		addToLists(root->left, lists, depth + 1);
+		addToLists(root->right, lists, depth + 1);
+	}
+}
+
+std::vector<std::list<int>> genLevelLinkedLists(TNode* root) {
+	std::vector<std::list<int>> lists;
+	addToLists(root, lists, 0);
+	return lists;
+}
+
+bool checkIfBST(TNode* root, int min, int max) {
+	if (root == NULL) {
+		return true;
+	}
+
+	if (root->data <= min || root->data > max) {
+		return false;
+	}
+
+	if (!checkIfBST(root->left, min, root->data) ||
+		!checkIfBST(root->right, root->data, max)) {
+		return false;
+	}
+	return true;
+}
+
+bool checkIfBST(TNode* root) {
+	checkIfBST(root, std::numeric_limits<int>::min(), std::numeric_limits<int>::max());
+}
+
+bool testOne() {
+	std::cout << "test1 ran" << std::endl;
+	return false;
+}
+
+bool testTwo() {
+	std::cout << "test2 ran" << std::endl;
+	return true;
+}
+
+TNode* findInOrderSuccessor(TNode* root) {
+	if (root->right) {
+		TNode* runner = root->right;
+		while (runner->left)
+			runner = runner->left;
+		return runner;
+	} else if (root->parent) {
+		TNode* runner = root;
+		while (runner->parent && runner->parent->left != runner) {
+			runner = runner->parent
+		}
+		return runner->parent;
+	}
+}
 
 int main() {
+	std::vector<int> v;
+	v.push_back(1);
+	v.push_back(2);
+	v.push_back(3);
+	v.push_back(4);
+	v.push_back(5);
+	v.push_back(6);
+	v.push_back(7);
+
+	TNode* root = genMinialHeightBST(v);
 	
+	// root->right->left->data = 2;
+
+	printBFSTree(root);
+
+	std::cout << checkIfBST(root) << std::endl;
+
+	// if (testOne() && testTwo()) {
+	// 	std::cout << "gg" << std::endl;
+	// }
 }
