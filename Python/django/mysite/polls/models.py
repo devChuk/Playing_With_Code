a195c161fe597bb3da0a42efa5d6379a4fcf85ee
root <brian_chuk@yahoo.com>
2017-06-07 00:34:48 -0400

    One more moment doctor

--- Python/django/mysite/polls/models.py
+++ Python/django/mysite/polls/models.py
@@ -1,6 +1,21 @@
 # -*- coding: utf-8 -*-
+"""question."""
 from __future__ import unicode_literals
 
 from django.db import models
 
+
+class Question(models.Model):
+    """question."""
+
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+class Choice(models.Model):
+    """choice."""
+
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
 # Create your models here.
