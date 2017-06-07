678399658d2395ee9994a3695b297d5ec22213a5
root <brian_chuk@yahoo.com>
2017-06-07 16:35:27 -0400

    jang

--- Python/django/mysite/polls/models.py
+++ Python/django/mysite/polls/models.py
@@ -3,10 +3,12 @@
 from __future__ import unicode_literals
 
 import datetime
+from django.utils.encoding import python_2_unicode_compatible
 from django.db import models
 from django.utils import timezone
 
 
+@python_2_unicode_compatible
 class Question(models.Model):
     """question."""
 
@@ -22,10 +24,15 @@ class Question(models.Model):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
+@python_2_unicode_compatible
 class Choice(models.Model):
     """choice."""
 
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        """Print func."""
+        return self.choice_text
 # Create your models here.
