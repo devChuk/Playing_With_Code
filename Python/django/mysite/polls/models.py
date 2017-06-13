f3a3670d0730e9e0c653d50b536b3c578050dfc0
root <brian_chuk@yahoo.com>
2017-06-13 16:25:48 -0400

    I test my code with strings of rap lyrics

--- Python/django/mysite/polls/models.py
+++ Python/django/mysite/polls/models.py
@@ -6,6 +6,29 @@ import datetime
 from django.utils.encoding import python_2_unicode_compatible
 from django.db import models
 from django.utils import timezone
+from simple_history.models import HistoricalRecords
+
+
+class ManualHistoricalRecords(HistoricalRecords):
+    def contribute_to_class(self, cls, name):
+        self.manager_name = name
+        self.module = cls.__module__
+        self.cls = cls
+        setattr(cls, 'skip_history_when_saving', True)
+        models.signals.class_prepared.connect(self.finalize, weak=False)
+        self.add_extra_methods(cls)
+
+    def add_extra_methods(self, cls):
+        def save_with_historical_record(self, *args, **kwargs):
+            """Save model with a historical record."""
+            delattr(self.__class__, 'skip_history_when_saving')
+            try:
+                ret = self.save(*args, **kwargs)
+            finally:
+                setattr(self.__class__, 'skip_history_when_saving', True)
+            return ret
+        setattr(cls, 'save_with_historical_record',
+                save_with_historical_record)
 
 
 @python_2_unicode_compatible
@@ -14,6 +37,7 @@ class Question(models.Model):
 
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    history = ManualHistoricalRecords()
 
     def __str__(self):
         """Print function."""
@@ -31,8 +55,14 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    history = ManualHistoricalRecords()
 
     def __str__(self):
         """Print func."""
         return self.choice_text
 # Create your models here.
+
+# from polls.models import Question, ManualHistoricalRecords, Choice
+# from django.utils import timezone
+# from simple_history.models import HistoricalRecords
+# q = Question(question_text="Do you remember me", pub_date=timezone.now())
