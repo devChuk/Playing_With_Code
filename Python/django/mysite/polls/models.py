88217eb625307c938199983a225053702372f462
root <brian_chuk@yahoo.com>
2017-06-30 15:41:55 -0400

    sasdfga

--- Python/django/mysite/polls/models.py
+++ Python/django/mysite/polls/models.py
@@ -26,7 +26,14 @@ class ManualHistoricalRecords(HistoricalRecords):
     def add_extra_methods(self, cls):
         def save_with_historical_record(self, *args, **kwargs):
             """Save model with a historical record."""
+            if 'history_snapshot' in kwargs:
+                snapshot = kwargs['history_snapshot']
+                del kwargs['history_snapshot']
+                print(snapshot)
+                print(self.question_text)
+
             self.skip_history_when_saving = False
+
             try:
                 ret = super(self.__class__, self).save(*args, **kwargs)
                 # we use super().save to not update changed_recently to True
@@ -38,12 +45,47 @@ class ManualHistoricalRecords(HistoricalRecords):
                 save_with_historical_record)
 
 
+def restore(instance, time):
+    historical_instance = instance.history.as_of(time)
+    # def restore(instance):
+    for field in instance._meta.get_fields():
+        if not field.auto_created:
+            print(field.name)
+            print(getattr(instance, field.name))
+            print(getattr(historical_instance, field.name))
+            setattr(instance, field.name, getattr(historical_instance, field.name))
+
+    instance.save()
+        # print(field.name)
+        # print(field)
+        # print(field.get_internal_type())
+        # print(field.related_model)
+        # print(field.model)
+        # print(field.auto_created)
+        # print(field.concrete)
+        # print(field.many_to_many)
+        # print('\n')
+        # print(getattr(instance, field.name))
+
+
+class Blahblah(models.Model):
+    dsaasd = models.CharField(max_length=200)
+    history = HistoricalRecords()
+
+    def __str__(self):
+        """Print func."""
+        return self.dsaasd
+
 class BaseModel(models.Model):
     history = ManualHistoricalRecords(inherit=True)
+    testFIeld = models.CharField(max_length=200, default='', blank=True,
+                                 null=True)
 
     class Meta:
         abstract = True
 
+
+# https://stackoverflow.com/questions/2608067/how-to-find-out-whether-a-models-column-is-a-foreign-key
 
 @python_2_unicode_compatible
 class Question(BaseModel):
@@ -63,7 +105,6 @@ class Question(BaseModel):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
     def save(self, *args, **kwargs):
-        print(self.pk is None)
         self.changed_recently = True
         super(Question, self).save(*args, **kwargs)
 
@@ -74,6 +115,9 @@ class Question(BaseModel):
 
     def completely_delete(self, *args, **kwargs):
         super(Question, self).delete(*args, **kwargs)
+
+    def normal_save(self, *args, **kwargs):
+        super(Question, self).save(*args, **kwargs)
 
 
 @python_2_unicode_compatible
@@ -89,7 +133,7 @@ class Choice(BaseModel):
         return self.choice_text
 # Create your models here.
 
-# from polls.models import Question, ManualHistoricalRecords, Choice
+# from polls.models import Question, ManualHistoricalRecords, Choice, restore
 # from django.utils import timezone
 # from simple_history.models import HistoricalRecords
 # q = Question(question_text="Do you remember me", pub_date=timezone.now())
